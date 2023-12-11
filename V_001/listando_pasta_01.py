18a695e9b37a39900f44984ec509cce51ecebdef
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-11 11:08:54 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -4,7 +4,14 @@ from re import search, match
 
 pasta_home = Path.home()
 subpastas = Path()
+arquivos = ['pdf', 'jpg']
+lista_arquivos = []
 
-for listando in listdir(pasta_home):
-    print(listando)
+for registrando in listdir(pasta_home):
+    lista_arquivos.append(registrando)
+
+for busca in lista_arquivos:
+    resultado = match(arquivos, busca)
+    if resultado:
+        print(busca)
 
