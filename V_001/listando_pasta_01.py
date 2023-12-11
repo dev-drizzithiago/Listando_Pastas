66c6dd36f2b8ecc1fbe967afe2bc7440a572bd72
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-11 12:18:37 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -1,14 +1,26 @@
 from os import listdir, chdir, getcwd
+# from tkinter import
 from re import search, match
 from pathlib import Path
 from os.path import isfile
 
-pasta_home = 'Z:\\Thiago\\Fotos\\Sexo'
 lista_arquivos_listados = []
 lista_pastas_listadas = []
 
-for c in listdir(pasta_home):
-    if search('jpg', c):
-        print(c)
-    elif search('mp4', c):
-        print(c)
+
+def locais():
+    caminho = 'Z:\\Thiago\\Fotos\\Sexo\\'
+    return caminho
+
+
+local_busca = locais()
+
+for listagem in listdir(local_busca):
+    if search('jpg', listagem):
+        print(listagem)
+    elif search('mp4', listagem):
+        print(listagem)
+    else:
+        sub_pasta = locais() + listagem
+        for listagem_sub in listdir(sub_pasta):
+            print(listagem_sub)
