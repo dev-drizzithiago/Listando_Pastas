410b3b85440c12b6e976410c32e8f013f55e04e8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-11 17:54:04 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -1,6 +1,5 @@
 from os import listdir, chdir, getcwd
 from time import sleep
-# from tkinter import
 from re import search, match
 from pathlib import Path
 from os.path import isfile
@@ -17,13 +16,5 @@ def locais():
 local_busca = locais()
 
 for listagem in listdir(local_busca):
-    if search('jpg', listagem):
-        print(listagem)
-    elif search('mp4', listagem):
-        print(listagem)
-    else:
-        sub_pasta = locais() + listagem
-        print(f'Listando diretorio {sub_pasta} ')
-        sleep(1)
-        for listagem_sub in listdir(sub_pasta):
-            print(listagem_sub)
+    print(listagem)
+
