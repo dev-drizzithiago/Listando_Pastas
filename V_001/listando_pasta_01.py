7f7dee7ffa6157d146b8913e9351784798701355
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-11 19:06:33 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -1,13 +1,9 @@
-from os import listdir, chdir, getcwd, walk
 from time import sleep
 from re import search, match
 from pathlib import Path
-from os.path import isfile, isdir
 
 lista_arquivos_listados = []
 lista_pastas_listadas = []
 
-caminho_busca = 'Z:\\Thiago\\Fotos\\'
-
-for path, diretorios, arquivos in walk(caminho_busca):
-    print(arquivos)
+caminho_busca = 'Z:\\Thiago\\Fotos\\Niver\\'
+busca_geral = listdir(caminho_busca)
