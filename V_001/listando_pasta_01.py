841c7b5cc83084f11782c0e563d9f7c65b859652
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-11 19:58:00 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -3,13 +3,18 @@ from re import search
 
 lista_arquivos_listados = []
 lista_pastas_listadas = []
+cont_arq = 1
+cont_path = 1
 
 caminho_busca = 'Z:\\Thiago\\'
 busca_geral = Path(caminho_busca)
 
 for valor_busca in busca_geral.glob('**/*'):
-    if valor_busca.is_file():
-        print(valor_busca.name)
-    elif valor_busca.is_dir():
+    if valor_busca.is_dir():
         print('\n')
-        print(f'Pasta - [{valor_busca.name}]')
+        nome_pasta = valor_busca.name
+    elif valor_busca.is_file():
+        nome_arquivo = valor_busca.name
+        print(f'{cont_arq} - {nome_arquivo}')
+        cont_arq += 1
+
