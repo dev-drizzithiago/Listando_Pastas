86039c436f7b6757fd573c2e0dd3dd211f600196
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-11 11:44:49 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -1,13 +1,14 @@
-from os import listdir
-from pathlib import Path
+from os import listdir, chdir, getcwd
 from re import search, match
+from pathlib import Path
+from os.path import isfile
 
-pasta_home = Path.home()
-subpastas = Path()
-arquivos = ['pdf']
-lista_arquivos = []
+pasta_home = 'Z:\\Thiago\\Fotos\\Sexo'
+lista_arquivos_listados = []
+lista_pastas_listadas = []
 
-for listando in listdir(pasta_home):
-    if search(arquivos, listando):
-        print(listando)
-
+for c in listdir(pasta_home):
+    if search('jpg', c):
+        print(c)
+    elif search('mp4', c):
+        print(c)
