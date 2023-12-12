ab0cac96243bbb13f426b2d4e70ce308d80f5118
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-11 21:25:59 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -3,10 +3,11 @@ from re import search
 
 lista_arquivos_listados = []
 lista_pastas_listadas = []
+cont_arq_total = 1
 cont_arq = 1
 cont_path = 1
 
-caminho_busca = 'Z:\\'
+caminho_busca = 'Z:\\Thiago\\Fotos\\Niver\\'
 busca_geral = Path(caminho_busca)
 
 for valor_busca in busca_geral.glob('**/*'):
@@ -14,8 +15,11 @@ for valor_busca in busca_geral.glob('**/*'):
         nome_pasta = valor_busca.name
         print(f'[{cont_path}] - [{nome_pasta}]')
         cont_path += 1
+        cont_arq += 1
     elif valor_busca.is_file():
         nome_arquivo = valor_busca.name
+        status_arq = valor_busca.stat().st_size
         print(f'<{cont_arq}> - <{nome_arquivo}>')
         cont_arq += 1
-
+        cont_arq_total += 1
+print(f'Foram encontrados ao total de {cont_path} arquivos em {cont_path} pastas')
