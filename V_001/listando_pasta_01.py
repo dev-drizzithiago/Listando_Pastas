1c28807302904b4994f6ba9bd2664d1a2858558e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-20 21:56:08 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -17,7 +17,6 @@ valor_arquivo = input('Digite a extensão do arquivo: ')
 caminho_busca = (diretorio())
 busca_geral = Path(caminho_busca)
 for valor_busca in busca_geral.glob('**/*'):
-    print(valor_busca)
     if valor_busca.is_dir():
         nome_pasta = valor_busca.name
         print(f'[{cont_path}] - [{nome_pasta}]')
