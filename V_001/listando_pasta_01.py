8ae0b17328848bdb66044f37dd230587c9b7a5a3
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-20 22:09:36 -0300

    Update listando_pasta_01.py

--- V_001/listando_pasta_01.py
+++ V_001/listando_pasta_01.py
@@ -17,19 +17,22 @@ def end_diretorio():
     return Path(caminho_busca)
 
 
+valor_arq_txt = open(end_diretorio(), 'r')
+
 valor_arquivo = input('Digite a extensão do arquivo: ')
 caminho_busca = (diretorio())
 busca_geral = end_diretorio()
 for valor_busca in busca_geral.glob('**/*.' + valor_arquivo):
     if valor_busca.is_dir():
         nome_pasta = valor_busca.name
-        print(f'[{cont_path}] - [{nome_pasta}]')
         cont_path += 1
         cont_arq += 1
+        print(f'[{cont_path}] - [{nome_pasta}]')
     elif valor_busca.is_file():
         nome_arquivo = valor_busca.name
         status_arq = valor_busca.stat().st_size
-        print(f'<{cont_arq}> - <{nome_arquivo}>')
         cont_arq += 1
         cont_arq_total += 1
+        valor_arq_txt.write(f'{cont_arq} - {nome_arquivo}')
+        print(f'<{cont_arq}> - <{nome_arquivo}>')
 print(f'Foram encontrados ao total de {cont_path} arquivos em {cont_path} pastas')
