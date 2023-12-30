77d53027aa8ce64c1cfd545870261c8fb35fc4c6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-30 15:58:57 -0300

    Update Listando_pasta_003.py

--- V_003/Listando_pasta_003.py
+++ V_003/Listando_pasta_003.py
@@ -23,28 +23,30 @@ class JanelaTK:
         caixa_entrada = tk.Label(self.janela_principal, text="Escolha uma extensão de arquivo")
         caixa_entrada.pack(anchor='n')
 
-        self.botao_entrar = tk.Button(self.frame_botao_001, text='Entrar', command=self.mensagem)
+        self.botao_entrar = tk.Button(self.frame_botao_001, text='Entrar', command=self.item_selecionado)
         self.botao_entrar.pack(anchor='s')
 
-        self.lista_arqs = tk.Listbox(self.frame_txt_02)
-        self.lista_arqs.insert(1, 'JPG')
-        self.lista_arqs.insert(2, 'MP4')
-        self.lista_arqs.insert(3, 'TXT')
-        self.lista_arqs.insert(0, 'ALL')
+        self.var_extersao = tk.Variable(value=self.extensoes)
+
+        self.lista_arqs = tk.Listbox(self.frame_txt_02, listvariable=self.var_extersao, selectmode=tk.EXTENDED)
+
         self.lista_arqs.pack(anchor='center')
         self.janela_principal.mainloop()
 
     def item_selecionado(self):
-        self.selecao = self.lista_arqs.get(str)
-        tk.messagebox.showinfo(self.selecao)
+        self.selecao = self.lista_arqs.curselection()
+        itens_selecionados = ','.join([self.lista_arqs.get(i) for i in self.selecao])
+        tk.messagebox.showinfo('AVISO', f'Você selecionou {self.selecao}')
+        self.lista_arqs.bind(itens_selecionados)
 
+        self.janela_principal.mainloop()
 
 
 obj_janela_busca = JanelaTK()
 
 
-def busca_pasta(self):
-    for listagem in pasta_busca.glob('**/*' + self.mensagem):
+def busca_pasta():
+    for listagem in pasta_busca.glob('**/*'):
         # print(listagem)
         if listagem.is_file():
             print(listagem)
