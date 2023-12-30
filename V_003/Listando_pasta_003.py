6d90b7a9bc4ff1e6034b0f5fc4f8c5e0f0a3cd29
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-30 12:19:51 -0300

    Update Listando_pasta_003.py

--- V_003/Listando_pasta_003.py
+++ V_003/Listando_pasta_003.py
@@ -1,11 +1,21 @@
 from pathlib import Path
-
+import tkinter as tk
 
 home = Path.home()
 
 pasta_busca = Path(home)
 
+
+class JanelaTK:
+    def __init__(self):
+        janela_principal = tk.Tk()
+        frame__01 = tk.Frame(janela_principal)
+        janela_principal.mainloop()
+
+
+obj_janela = JanelaTK()
+
 for listagem in pasta_busca.glob('**/*'):
     # print(listagem)
     if listagem.is_file():
-        print(listagem.name)
+        print(listagem)
