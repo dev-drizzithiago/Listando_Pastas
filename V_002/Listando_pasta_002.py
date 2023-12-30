e4c1149c160a72f076a9cd212aa30c964901b5a3
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2023-12-30 11:17:40 -0300

    Create Listando_pasta_002.py

---
+++ V_002/Listando_pasta_002.py
@@ -0,0 +1,13 @@
+from tkinter.filedialog import askdirectory, asksaveasfilename
+import tkinter
+
+
+class ListandoPasta:
+    def __init__(self):
+        janela_principal = tkinter.Tk()
+
+
+        janela_principal.mainloop()
+
+
+obj_listando = ListandoPasta()
