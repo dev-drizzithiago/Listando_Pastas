85337a3fbf8ef920edf61030ab5804c1a93e431a
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-01 20:04:45 -0300

    Update Listando_pasta_004.py

--- V_004/Listando_pasta_004.py
+++ V_004/Listando_pasta_004.py
@@ -13,7 +13,14 @@ class ListandoPastas:
 
         # Frames
         self.frame_label_001 = tk.LabelFrame(self.janela_principal, text='Escolha uma opção')
-        self.frame_label_001.pack(anchor='center', fill='both')
+        self.frame_label_001.pack(fill='both')
+
+        self.frame_txt_001 = tk.Frame(self.frame_label_001)
+        self.frame_txt_002 = tk.Frame(self.frame_label_001)
+        self.frame_txt_001.pack(side='top')
+        self.frame_txt_002.pack(side='bottom')
+        self.label_principal = tk.Label(self.frame_label_001, text='Escolha um arquivo')
+        self.label_principal.pack(anchor='center')
 
         self.janela_principal.mainloop()
 
