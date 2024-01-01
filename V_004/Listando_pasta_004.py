f1b3b48c0c4f76e5db9483140a87e7d13ddeb464
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-01 19:57:04 -0300

    Update Listando_pasta_004.py

--- V_004/Listando_pasta_004.py
+++ V_004/Listando_pasta_004.py
@@ -1,11 +1,21 @@
 from tkinter.filedialog import askopenfilename
-import tkinter as tk
+from pathlib import Path
 from time import sleep
+import tkinter as tk
+
 
 class ListandoPastas:
     def __init__(self):
         # Janela principal
         self.janela_principal = tk.Tk()
         self.janela_principal.geometry('300x300')
-        self.janela_principal.title(f'Buscando arquivos em na pasta {self.pasta_destino}')
+        self.janela_principal.title('Buscando arquivos em na pasta')
 
+        # Frames
+        self.frame_label_001 = tk.LabelFrame(self.janela_principal, text='Escolha uma opção')
+        self.frame_label_001.pack(anchor='center', fill='both')
+
+        self.janela_principal.mainloop()
+
+
+obj_listando_pasta = ListandoPastas()
