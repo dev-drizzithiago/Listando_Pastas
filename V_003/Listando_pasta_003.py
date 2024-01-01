86cafa675e343dc95e6eee2465177f952b020e88
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-01 19:17:02 -0300

    Update Listando_pasta_003.py

--- V_003/Listando_pasta_003.py
+++ V_003/Listando_pasta_003.py
@@ -10,6 +10,8 @@ class JanelaTK:
         # GERAL
         self.pasta_destino = Path(Path.home())
         self.extensoes = ['TUDO', 'JPG', 'MP4', 'TXT']
+        self.linhas_aparencia = '-=-' * 30
+        # Janela principal
         self.janela_principal = tk.Tk()
         self.janela_principal.geometry('400x400')
 
@@ -21,6 +23,8 @@ class JanelaTK:
 
         self.frame_botao_001 = tk.Frame(self.janela_principal)
         self.frame_botao_001.pack(anchor='s')
+        self.frame_botao_sair = tk.Frame(self.janela_principal)
+        self.frame_botao_sair.pack(anchor='se')
 
         # VARIAVEIS
         self.var_extersao = tk.Variable(value=self.extensoes)
@@ -36,7 +40,8 @@ class JanelaTK:
         self.botao_entrar.pack(side='left')
         self.botao_destino = tk.Button(self.frame_botao_001, text='Escolher Caminho', command=self.diretorio)
         self.botao_destino.pack(side='right')
-        self.botao_sair = tk.Button(self.frame_botao_001, text='Sair', command=self.janela_principal.destroy)
+        self.botao_sair = tk.Button(self.frame_botao_sair, text='Sair', padx=5, pady=5,
+                                    command=self.janela_principal.destroy)
         self.botao_sair.pack(side='bottom')
 
         self.janela_principal.mainloop()
@@ -58,7 +63,10 @@ class JanelaTK:
 
     def buscando_arquivos(self):
         print('inciando busca...')
+        sleep(1)
+        print(self.linhas_aparencia)
         print('Buscando arquivos ', self.opcao_arquivo)
+        print(self.linhas_aparencia)
         for listagem in self.pasta_destino.glob('**/*' + self.opcao_arquivo):
             if listagem.is_file():
                 print(listagem)
