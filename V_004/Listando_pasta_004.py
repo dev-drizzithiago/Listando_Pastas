9c6f9cece377b91016130f8490981d85bb23eaa0
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-01 20:11:13 -0300

    Update Listando_pasta_004.py

--- V_004/Listando_pasta_004.py
+++ V_004/Listando_pasta_004.py
@@ -11,16 +11,25 @@ class ListandoPastas:
         self.janela_principal.geometry('300x300')
         self.janela_principal.title('Buscando arquivos...')
 
-        # Frames
-        self.frame_label_001 = tk.LabelFrame(self.janela_principal, text='Escolha uma opção')
+        # Frames Label
+        self.frame_label_001 = tk.LabelFrame(self.janela_principal, text='Arquivos Disponível')
+        self.frame_label_002 = tk.LabelFrame(self.janela_principal, text='Botões')
         self.frame_label_001.pack(fill='both')
+        self.frame_label_002.pack(fill='both')
 
+        # Frames
         self.frame_txt_001 = tk.Frame(self.frame_label_001)
         self.frame_txt_002 = tk.Frame(self.frame_label_001)
         self.frame_txt_001.pack(side='top')
         self.frame_txt_002.pack(side='bottom')
-        self.label_principal = tk.Label(self.frame_label_001, text='Escolha um arquivo')
-        self.label_principal.pack(anchor='center')
+
+        # Botoes
+        self.botao_entrar = tk.Button(self.frame_label_002, text='Entrar')
+        self.botao_entrar.pack(side='right')
+
+
+
+
 
         self.janela_principal.mainloop()
 
