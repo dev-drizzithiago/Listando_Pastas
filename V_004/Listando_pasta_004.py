c57eecc414d7681ad2c9d9af93381af5d0f3d3f2
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-01 21:26:09 -0300

    Update Listando_pasta_004.py

--- V_004/Listando_pasta_004.py
+++ V_004/Listando_pasta_004.py
@@ -13,46 +13,53 @@ class ListandoPastas:
         self.janela_principal = tk.Tk()
         self.janela_principal.geometry('300x300')
         self.janela_principal.title('Listagem de arquivos')
-        # janela listagem
-        self.janela_listagem = tk.Tk()
-        self.janela_listagem.geometry('300x300')
-        self.janela_listagem.title('Listando arquivos...')
 
         # Frames Label
-        self.frame_label_001 = tk.LabelFrame(self.janela_principal, text='Selecione um arquivo', pady=3, padx=3)
-        self.frame_label_002 = tk.LabelFrame(self.janela_principal, padx=3, pady=3)
-        self.frame_label_003 = tk.LabelFrame(self.janela_listagem, text='Arquivos listados', padx=3, pady=3)
-        self.frame_label_004 = tk.LabelFrame(self.janela_listagem, pady=3, padx=3)
-        self.frame_label_001.pack(fill='both')
-        self.frame_label_002.pack(fill='both')
-        self.frame_label_003.pack(fill='both')
-        self.frame_label_004.pack(fill='both')
+        self.frame_label_principal_001 = tk.LabelFrame(self.janela_principal, text='Selecione um arquivo', pady=3, padx=3)
+        self.frame_label_principal_002 = tk.LabelFrame(self.janela_principal, padx=3, pady=3)
+        self.frame_label_principal_001.pack(fill='both')
+        self.frame_label_principal_002.pack(fill='both')
 
         # Frames
-        self.frame_txt_001 = tk.Frame(self.frame_label_001, pady=3, padx=3)
-        self.frame_txt_002 = tk.Frame(self.frame_label_001, pady=3, padx=3)
+        self.frame_txt_001 = tk.Frame(self.frame_label_principal_001, pady=3, padx=3)
+        self.frame_txt_002 = tk.Frame(self.frame_label_principal_001, pady=3, padx=3)
         self.frame_txt_001.pack(side='top')
         self.frame_txt_002.pack(side='bottom')
 
         # Lista
         self.variavel_extensao = tk.Variable(value=self.extensoes_arquivos_imagem)
-        self.lista_extensoes_001 = tk.Listbox(self.frame_label_001, listvariable=self.variavel_extensao, justify='center')
+        self.lista_extensoes_001 = tk.Listbox(self.frame_label_principal_001, listvariable=self.variavel_extensao, justify='center')
         self.lista_extensoes_001.pack(fill='both', expand='yes')
 
-        # Mesangem
-        self.caixa_txt_001 = tk.Message(self.frame_label_003)
-        self.caixa_txt_001.pack(anchor='center')
+
 
         # Botoes
-        self.botao_entrar = tk.Button(self.frame_label_002, text='Selecionar', command=self.iniciando_busca_arquivos)
+        self.botao_entrar = tk.Button(self.frame_label_principal_002, text='Selecionar', command=self.iniciando_busca_arquivos)
         self.botao_entrar.pack(side='left')
-        self.botao_sair_principal = tk.Button(self.frame_label_002, text='Sair', command=self.janela_principal.destroy)
+        self.botao_sair_principal = tk.Button(self.frame_label_principal_002, text='Sair', command=self.janela_principal.destroy)
         self.botao_sair_principal.pack(side='right')
-        self.botao_sair_listagem = tk.Button(self.frame_label_004, padx=3, pady=3)
+
 
         self.janela_principal.mainloop()
 
     def iniciando_busca_arquivos(self):
+        # janela listagem
+        self.janela_listagem = tk.Tk()
+        self.janela_listagem.geometry('300x300')
+        self.janela_listagem.title('Listando arquivos...')
+        # Frames
+        self.frame_label_listagem_001 = tk.LabelFrame(self.janela_listagem, text='Arquivos listados', padx=3, pady=3)
+        self.frame_label_listagem_002 = tk.LabelFrame(self.janela_listagem, pady=3, padx=3)
+        self.frame_label_listagem_001.pack(fill='both')
+        self.frame_label_listagem_002.pack(fill='both')
+
+        # Mesangem
+        self.caixa_txt_001 = tk.Message(self.frame_label_listagem_001, pady=3, padx=3, relief='raised')
+        self.caixa_txt_001.pack(anchor='center')
+        # Botao
+        self.botao_sair_listagem = tk.Button(self.frame_label_listagem_002, padx=3, pady=3)
+        self.botao_sair_listagem.pack(anchor='se')
+
         tipo_arquivo = self.lista_extensoes_001.curselection()
         for extensao_arq in tipo_arquivo:
             print(self.extensoes_arquivos_imagem[extensao_arq])
