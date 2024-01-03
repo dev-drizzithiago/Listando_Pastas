20df06344e2ce1958d0946851cb61e876e626c03
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-03 09:19:05 -0300

    Update Listagem_pasta_005.py

--- V_005/Listagem_pasta_005.py
+++ V_005/Listagem_pasta_005.py
@@ -1,17 +1,22 @@
 import tkinter as tk
 
+
 class ListagemPasta:
     def __init__(self):
         self.janela_principal = tk.Tk()
         self.janela_principal.geometry('600x300')
         self.janela_principal.title('Versão 5')
 
-        self.frame_principal_01 = tk.Frame(self.janela_principal)
-        self.frame_principal_02 = tk.Frame(self.janela_principal)
-        self.frame_principal_01.pack()
-        self.frame_principal_02.pack()
+        self.label_principal_01 = tk.LabelFrame(self.janela_principal, text='Escolha um tipo de extensão')
+        self.label_principal_02 = tk.LabelFrame(self.janela_principal)
+        self.label_principal_01.pack()
+        self.label_principal_02.pack()
 
 
 
+        self.botao_adicionar_01 = tk.Button()
 
-        self.janela_principa.mainloop()
+        self.janela_principal.mainloop()
+
+
+inicio_obj = ListagemPasta()
