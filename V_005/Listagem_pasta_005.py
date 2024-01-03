c8148a83cbe227e14ec7e80f3a9d1fab5d7e12c8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-03 10:28:13 -0300

    Update Listagem_pasta_005.py

--- V_005/Listagem_pasta_005.py
+++ V_005/Listagem_pasta_005.py
@@ -13,7 +13,8 @@ class ListagemPasta:
         self.janela_principal.geometry('600x300')
         self.janela_principal.title('Versão 5')
 
-        self.label_principal_01 = tk.LabelFrame(self.janela_principal, text='Escolha um tipo de extensão', pady=3, padx=3)
+        self.label_principal_01 = tk.LabelFrame(self.janela_principal, text='Escolha um tipo de extensão', pady=3,
+                                                padx=3)
         self.label_principal_02 = tk.LabelFrame(self.janela_principal)
         self.label_principal_01.pack()
         self.label_principal_02.pack()
@@ -22,7 +23,8 @@ class ListagemPasta:
         self.lista_extensoes_dispo = tk.Listbox(self.label_principal_01, listvariable=valor_extensoes, justify='center')
         self.lista_extensoes_dispo.pack(anchor='center')
 
-        self.botao_adicionar_01 = tk.Button(self.label_principal_02, text='Adicionar extensões', command=self.add_extensao)
+        self.botao_adicionar_01 = tk.Button(self.label_principal_02, text='Adicionar extensões',
+                                            command=self.add_extensao)
         self.botao_adicionar_01.pack(side='left')
         self.botao_iniciar_programa = tk.Button(self.label_principal_02, text='Iniciar programa')
         self.botao_iniciar_programa.pack(side='right')
@@ -33,10 +35,9 @@ class ListagemPasta:
         try:
             valor_dados_add = tk.simpledialog.askstring('Bem vindo!', 'Adicione uma extensão')
             self.registrar_extensao(valor_dados_add)
-            tk.messagebox.showinfo('AVISO', f'Extensão [{valor_dados_add.upper()}] adicionada com sucesso!')
+            # tk.messagebox.showinfo('AVISO', f'Extensão [{valor_dados_add.upper()}] adicionada com sucesso!')
         except:
-            tk.messagebox.showerror('AVISO', 'Não foi possível adicionar a extensão')
-
+            tk.messagebox.showerror('AVISO', f'Não foi possível adicionar a extensão {valor_dados_add}')
 
     def registrar_extensao(self, valor_registro):
         try:
@@ -46,5 +47,6 @@ class ListagemPasta:
             tk.messagebox.showinfo('AVISO', 'Extensão {} foi adicionada com sucesso!')
         except:
             tk.messagebox.showerror('AVISO!', 'Não foi possível registrar nos arquivo')
+
 
 inicio_obj = ListagemPasta()
