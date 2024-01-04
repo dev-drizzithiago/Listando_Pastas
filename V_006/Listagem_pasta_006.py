0f4433916c0074352ed51eb6abe7d6db568530d3
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-04 15:54:30 -0300

    Update Listagem_pasta_006.py

--- V_006/Listagem_pasta_006.py
+++ V_006/Listagem_pasta_006.py
@@ -10,13 +10,13 @@ class JanelaTeste:
 
         # Caixa de combinação
         self.current_var = tk.StringVar()
-        self.current_var.get()
-        self.current_var.set(value=self.tipo_midia)
         self.lista_de_midias = ttk.Combobox(self.janela_principal, textvariable=self.current_var, justify='center')
         self.lista_de_midias['values'] = self.tipo_midia
         self.lista_de_midias.set('Tipos de arquivos disponível')
         self.lista_de_midias.pack(fill='both', side='top')
         self.lista_de_midias.current()
+
+        self.current_var.trace('w', self.item_selecionado)
 
         self.botao_limpar = tk.Button(self.janela_principal, text='Limpar', command=self.limpeza)
         self.botao_limpar.pack(side='bottom', padx=5, pady=5)
@@ -26,7 +26,7 @@ class JanelaTeste:
     def limpeza(self):
         self.lista_de_midias.set('')
 
-    def item_selecionado(self):
+    def item_selecionado(self, *args):
         print(self.current_var.get())
 
 
