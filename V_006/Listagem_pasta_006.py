93c848b57e4bb4de827d90877c402f7e80404a98
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-04 15:30:07 -0300

    Update Listagem_pasta_006.py

--- V_006/Listagem_pasta_006.py
+++ V_006/Listagem_pasta_006.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 
+
 class JanelaTeste:
     def __init__(self):
-        self.janela_principal = tk.Tk
+        self.janela_principal = tk.Tk()
         self.janela_principal.geometry('300x300')
         self.tipo_midia = ('Vídes', 'Imagem')
 
-# Caixa de combinação
+        # Caixa de combinação
         self.current_var = tk.StringVar()
         self.current_var.get()
         self.current_var.set(value=self.tipo_midia)
@@ -16,3 +17,8 @@ class JanelaTeste:
         self.lista_de_midias.set('Tipos de arquivos disponível')
         self.lista_de_midias.pack(fill='both', side='top')
         self.lista_de_midias.current()
+
+        self.janela_principal.mainloop()
+
+
+iniciando_programa = JanelaTeste()
