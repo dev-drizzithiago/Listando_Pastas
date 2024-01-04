a2b368c7a68323ba1e7fc524588d6a5a0775d698
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-04 16:34:26 -0300

    Update Listagem_pasta_007.py

--- V_007/Listagem_pasta_007.py
+++ V_007/Listagem_pasta_007.py
@@ -1,10 +1,12 @@
 from tkinter import *
 from tkinter.ttk import *
+from pathlib import Path
 
 
 class ListagemPastas:
     def __init__(self):
         # Varias Global
+        self.pasta_home = Path.home()
         self.lista_tipos_extensoes = ('VÍDEOS', 'IMAGENS', 'ARQUIVOS_TXT')
 
         # Janela principa
@@ -18,24 +20,29 @@ class ListagemPastas:
         self.label_frame_002 = Frame(self.janela_principal)
         self.label_frame_003 = Frame(self.janela_principal)
         self.label_frame_004 = Frame(self.janela_principal)
-        self.label_frame_001.pack()
-        self.label_frame_002.pack()
+        self.label_frame_001.pack(anchor='n', fill='both')
+        self.label_frame_002.pack(anchor='center', fill='both')
 
-        self.label_titulo_principal = Label(self.janela_principal, text='MERCURTY TI', justify='center')
+        self.label_titulo_principal = Label(self.label_frame_001, text='MERCURTY TI', justify='center')
         self.label_titulo_principal.pack(side='top', fill='both')
 
         # Lista Combo
         self.var_cambo = StringVar()
-        self.combo_tipo_arquivo = Combobox(self.janela_principal, textvariable=self.var_cambo, justify='center')
+        self.combo_tipo_arquivo = Combobox(self.label_frame_001, textvariable=self.var_cambo, justify='center')
         self.combo_tipo_arquivo['values'] = self.lista_tipos_extensoes
         self.combo_tipo_arquivo.set('Escolha um tipo de Mídia')
         self.combo_tipo_arquivo.pack(side='top', fill='both')
         self.combo_tipo_arquivo.current()
         self.var_cambo.trace('w', self.opcao_midia)
 
+        # Lista de exntesão
+        self.lista_extensão = Listbox(self.label_frame_002, justify='center')
+        self.lista_extensão.pack(side='top', fill='both', padx=5, pady=5)
+
         self.janela_principal.mainloop()
 
     def opcao_midia(self, *args):
         print(self.combo_tipo_arquivo.get())
+
 
 obj_principal = ListagemPastas()
