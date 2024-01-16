3ccd8c61c0dd90f531477de4b4032d6203e0db9d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-16 20:56:28 -0300

    Create V_008.py

---
+++ V_008/V_008.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.simpledialog import *
+from tkinter.messagebox import *
+
+
+class ListandoPastas:
+    def __init__(self):
+        janela_principal = Tk()
+        janela_principal.geometry('350x390')
+        janela_principal.config(padx=5, pady=5)
+        janela_principal.mainloop()
+
+
+obj_principal = ListandoPastas()
+
