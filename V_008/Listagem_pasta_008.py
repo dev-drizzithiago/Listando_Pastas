aec6d416cabd739946bdbd22dbf7ac642f9fccb7
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-17 10:53:55 -0300

    Update Listagem_pasta_008.py

--- V_008/Listagem_pasta_008.py
+++ V_008/Listagem_pasta_008.py
@@ -40,13 +40,12 @@ class ListandoPastas:
         label_hora.pack(anchor='center')
 
         # Combo
-        self.variavel_combo = StringVar()
         label_frame_combo = LabelFrame(janela_principal, text='Categorias de arquivos')
         label_frame_combo.pack(fill=BOTH)
-        combo_principal = Combobox(label_frame_combo, textvariable=self.variavel_combo, justify='center')
-        combo_principal['values'] = self.categorias
-        combo_principal.set('Escolha uma categoria')
-        combo_principal.pack(fill=BOTH, pady=5, padx=5)
+        self.combo_principal = Combobox(label_frame_combo, justify='center')
+        self.combo_principal['values'] = self.categorias
+        self.combo_principal.set('Escolha uma categoria')
+        self.combo_principal.pack(fill=BOTH, pady=5, padx=5)
 
         # lista principal
         self.variavel_lista_principal = IntVar()
@@ -108,7 +107,7 @@ class ListandoPastas:
         lista_busca.pack(fill=BOTH, anchor='center', padx=5, pady=5)
 
     def combo_selecao_categoria(self):
-        valor_categoria = self.variavel_combo.get()
+        valor_categoria = self.combo_selecao_categoria.get()
         print(valor_categoria)
 
 
