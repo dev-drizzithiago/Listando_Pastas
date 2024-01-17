88a4f989b72f375614b202515a66ebc9cabe0712
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-16 21:26:17 -0300

    Update V_008.py

--- V_008/V_008.py
+++ V_008/V_008.py
@@ -11,15 +11,19 @@ hora_certa = valor_hora.strftime('%H:%M')
 
 class ListandoPastas:
     def __init__(self):
+
+        # janela princpal
         janela_principal = Tk()
         janela_principal.geometry('350x390')
         janela_principal.config(padx=5, pady=5)
 
+        # Relogio
         label_frame_relogio = LabelFrame(janela_principal, text='Hora Certa')
         label_frame_relogio.pack(anchor='center')
         label_hora = Label(label_frame_relogio, text=f'{data_certa} - {hora_certa}')
         label_hora.pack(anchor='center')
 
         janela_principal.mainloop()
-obj_principal = ListandoPastas()
 
+
+obj_principal = ListandoPastas()
