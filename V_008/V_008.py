c52bbb73b4b223f5ae18f7bbbdf84488f84e53f7
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-16 21:04:26 -0300

    Update V_008.py

--- V_008/V_008.py
+++ V_008/V_008.py
@@ -1,7 +1,12 @@
 from tkinter import *
 from tkinter.ttk import *
+from datetime import datetime
 from tkinter.simpledialog import *
 from tkinter.messagebox import *
+
+valor_hora = datetime.now()
+data_certa = valor_hora.strftime('%D/%M/%Y')
+hora_certa = valor_hora.strftime('%H:%M')
 
 
 class ListandoPastas:
@@ -9,8 +14,12 @@ class ListandoPastas:
         janela_principal = Tk()
         janela_principal.geometry('350x390')
         janela_principal.config(padx=5, pady=5)
+
+        label_frame_relogio = LabelFrame(janela_principal, text='Hora Certa')
+        label_frame_relogio.pack(anchor='center')
+        label_hora = Label(label_frame_relogio, text=f'{data_certa} - {hora_certa}')
+        label_hora.pack(anchor='center')
+
         janela_principal.mainloop()
-
-
 obj_principal = ListandoPastas()
 
