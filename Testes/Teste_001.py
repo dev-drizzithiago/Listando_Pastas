1a0c42d94717dea2f5ef594aa4adc11e44fe5d34
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-18 22:31:22 -0300

    Update Teste_001.py

--- Testes/Teste_001.py
+++ Testes/Teste_001.py
@@ -1,3 +1,4 @@
+from threading import Thread
 from tkinter.ttk import *
 from tkinter import *
 import time
@@ -7,17 +8,21 @@ ws.title('PythonGuides')
 ws.geometry('400x250+1000+300')
 
 
+def inicio_step():
+    Thread(target=step()).start()
+
+
 def step():
-    for i in range(5):
+    for i in range(500):
         ws.update_idletasks()
         pb1['value'] += 20
 
-        time.sleep(1)
+        time.sleep(0.1)
 
 
 pb1 = Progressbar(ws, orient=HORIZONTAL, length=100, mode='indeterminate')
 pb1.pack(expand=True)
 
-Button(ws, text='Start', command=step).pack()
+Button(ws, text='Start', command=inicio_step).pack()
 
 ws.mainloop()
