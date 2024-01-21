25437545b77a5c84c4035c335beb5d3005b90feb
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-21 19:36:56 -0300

    Update Teste_002.py

--- Testes/Teste_002.py
+++ Testes/Teste_002.py
@@ -1,15 +1,23 @@
 import tkinter as tk
 
-janela_princial = tk.Tk()
-janela_princial.geometry('300x300')
-frame_001 = tk.Frame(janela_princial)
-frame_001.pack(fill='both')
-var_label = tk.StringVar
-label_001 = tk.Label(frame_001, textvariable=var_label.get, border=5)
-label_001.pack(fill='both')
 
-botao_atualizacao = tk.Button(frame_001, text='Atualizar')
-botao_atualizacao.pack()
+class Teste_002:
+    def __init__(self):
+        self.janela_princial = tk.Tk()
+        self.janela_princial.geometry('300x300')
+        self.frame_001 = tk.Frame(self.janela_princial)
+        self.frame_001.pack(fill='both')
+        self.var_label = tk.StringVar()
+        self.label_001 = tk.Label(self.frame_001, textvariable=self.var_label.get(), border=5)
+        self.label_001.pack(fill='both')
+
+        botao_atualizacao = tk.Button(self.frame_001, text='Atualizar', command=self.atualizar)
+        botao_atualizacao.pack()
+
+        self.janela_princial.mainloop()
+
+    def atualizar(self):
+        self.var_label.set('teste')
 
 
-janela_princial.mainloop()
+obj_iniciar = Teste_002()
