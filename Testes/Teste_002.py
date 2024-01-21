d38ec45aa5875bd80234fd227b6c33e7fd32bc2d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-21 19:46:34 -0300

    Update Teste_002.py

--- Testes/Teste_002.py
+++ Testes/Teste_002.py
@@ -5,13 +5,16 @@ class Teste_002:
     def __init__(self):
         self.janela_princial = tk.Tk()
         self.janela_princial.geometry('300x300')
-        self.frame_001 = tk.Frame(self.janela_princial)
+        self.frame_001 = tk.Frame(self.janela_princial, bg='green')
         self.frame_001.pack(fill='both')
+        self.frame_002 = tk.Frame(self.janela_princial, bg='blue')
+        self.frame_002.pack(fill=tk.BOTH)
         self.var_label = tk.StringVar()
-        self.label_001 = tk.Label(self.frame_001, textvariable=self.var_label.get(), border=5)
-        self.label_001.pack(fill='both')
+        self.var_label.set('teste')
+        self.label_001 = tk.Label(self.frame_001, textvariable=self.var_label.get(), border=1, bd=5)
+        self.label_001.pack(fill='both', pady=5, padx=5)
 
-        botao_atualizacao = tk.Button(self.frame_001, text='Atualizar', command=self.atualizar)
+        botao_atualizacao = tk.Button(self.frame_002, text='Atualizar', command=self.atualizar)
         botao_atualizacao.pack()
 
         self.janela_princial.mainloop()
