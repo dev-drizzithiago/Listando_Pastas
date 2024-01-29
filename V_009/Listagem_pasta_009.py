e1d97acc4102b8145c7be1e1b85e13dba6cc6bb6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-01-29 18:10:36 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
 from time import sleep
+from threading import Thread
 
 
 class ListandoArquivos:
@@ -12,12 +13,15 @@ class ListandoArquivos:
         self.label_frame_geral = LabelFrame(self.janela_principal, text='Janela Principal')
         self.label_frame_geral.pack(fill=BOTH, ipadx=5, ipady=5)
 
-        self.botao_iniciar_busca = Button(self.label_frame_geral, text='Iniciar Busca')
+        self.botao_iniciar_busca = Button(self.label_frame_geral, text='Iniciar Busca', command=self.iniciar_busca)
         self.botao_iniciar_busca.pack(anchor='center')
 
         self.janela_principal.mainloop()
 
     def thread_botao_iniciar(self):
+        Thread(target=self.iniciar_busca()).start()
+
+    def iniciar_busca(self):
         print('iniciando a busca')
         for valor in range(1, 10):
             print(valor)
