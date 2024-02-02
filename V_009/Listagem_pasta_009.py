c5cb48c4f1d90628207b1f9928eda75406427e52
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-02 12:31:37 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -111,38 +111,38 @@ class ListandoArquivos:
 
         # LABEM FRAME INFO BUSCA
         self.label_frame_geral_info = tk.LabelFrame(self.label_frame_geral, text='Informações da busca...!')
-        self.label_frame_geral_info.config(border=2, bd=2, relief=tk.RIDGE)
+        self.label_frame_geral_info.config(relief=tk.RIDGE)
         self.label_frame_geral_info.pack(anchor='center', fill='both', pady=5, padx=5)
 
         # LABEL STATUS GERAL
         self.var_label_status_geral = tk.StringVar()
         self.label_status = tk.Label(self.label_frame_geral_info, text=self.var_label_status_geral.get())
-        self.label_status.config(justify='center', border=2, bd=2, relief=tk.RIDGE)
-        self.label_status.pack(anchor='s', fill='both', pady=5, padx=5)
+        self.label_status.config(justify='center', relief=tk.RIDGE)
+        self.label_status.pack(anchor='s', pady=5, padx=5)
 
         # LABEL CONTAGEM ARQUIVOS
         self.var_status_contagem_arquivos = tk.StringVar()
         self.status_contagem_arquivos = tk.Label(self.label_frame_geral_info)
         self.status_contagem_arquivos.config(text=self.var_status_contagem_arquivos.get())
-        self.status_contagem_arquivos.config(justify='center', border=2, bd=2, relief=tk.RIDGE)
-        self.status_contagem_arquivos.pack(anchor='s', fill='both', pady=5, padx=5)
+        self.status_contagem_arquivos.config(justify='center', relief=tk.RIDGE)
+        self.status_contagem_arquivos.pack(anchor='s', pady=5, padx=5)
 
         # LABEL CONTAGEM PASTAS
         self.var_status_contagem_pastas = tk.StringVar()
         self.status_contagem_pastas = tk.Label(self.label_frame_geral_info, text=self.var_status_contagem_pastas.get())
-        self.status_contagem_pastas.config(justify='center', border=2, bd=2, relief=tk.RIDGE)
-        self.status_contagem_pastas.pack(anchor='s', fill='both', pady=5, padx=5)
+        self.status_contagem_pastas.config(justify='center', relief=tk.RIDGE)
+        self.status_contagem_pastas.pack(anchor='s', pady=5, padx=5)
 
         # LABEL CONTAGEM GERAL ARQUIVOS E PASTAS
         self.var_msg_tot_busca = tk.StringVar()
         self.msg_tot_busca = tk.Label(self.label_frame_geral_info, text=self.var_msg_tot_busca.get())
-        self.msg_tot_busca.config(justify='center', border=2, bd=2, relief=tk.RIDGE)
-        self.msg_tot_busca.pack(anchor='s', fill='both', pady=5, padx=5)
+        self.msg_tot_busca.config(justify='center', relief=tk.RIDGE)
+        self.msg_tot_busca.pack(anchor='s', pady=5, padx=5)
 
         # LABEL TIME DA BUSCA
-        self.var_label_time_busca = tk.StringVar()
-        self.label_time_busca = tk.Label(self.label_frame_geral_info, text=self.var_label_time_busca.get())
-        self.label_time_busca.pack(anchor='s', fill='both', pady=5, padx=5)
+        # self.var_label_time_busca = tk.StringVar()
+        # self.label_time_busca = tk.Label(self.label_frame_geral_info, text=self.var_label_time_busca.get())
+        # self.label_time_busca.pack(anchor='s', fill='both', pady=5, padx=5)
 
         # _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
         # BOTÕES
