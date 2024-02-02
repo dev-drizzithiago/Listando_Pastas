5e699398c52636f2e0f15e2d57fdc1e94c9cf3be
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-02 20:30:10 -0300

    Update Teste_005.py

--- Testes/Teste_005.py
+++ Testes/Teste_005.py
@@ -1,4 +1,11 @@
 from tkinter import *
 from tkinter.ttk import *
 
+janela_principal = Tk()
+janela_principal.geometry('500x450+400+300')
+janela_principal.title('Cronometro')
+janela_principal.config(bg='#299617')
+janela_principal.resizable(0,0)
 
+
+janela_principal.mainloop()
