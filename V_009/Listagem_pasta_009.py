577b2f400b9fe7daec2d37b16d142a18941af747
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-05 11:12:29 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -274,8 +274,9 @@ class ListandoArquivos:
                         msg_info_time = str(f'00:0{contagem_minutos}:{contagem_segundos}')
                     elif contagem_segundos < 10 and contagem_minutos > 9 and contagem_horas == 0:
                         msg_info_time = str(f'00:{contagem_minutos}:0{contagem_segundos}')
-                    elif contagem_segundos > 9 and contagem_minutos > 9 and contagem_horas == 0
-                    
+                    elif contagem_segundos > 9 and contagem_minutos > 9 and contagem_horas == 0:
+                        msg_info_time = str(f'00:{contagem_minutos}:{contagem_segundos}')
+
                     elif contagem_segundos < 10 and contagem_minutos < 10 and contagem_horas < 10:
                     if contagem_segundos == 59:
                         print('teste')
