66ede8c9b2271efac327f05fd403a527675ae388
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-07 09:24:28 -0300

    Update Teste_007.py

--- Testes/Teste_007.py
+++ Testes/Teste_007.py
@@ -1,8 +1,19 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
+"""Uma folha A4 é composta por 595,2 pontos de largura ( largura ) e 841,8 pontos de altura ( altura )."""
+
 w, h = A4
 c = canvas.Canvas('ola mundo.pdf', pagesize=A4)
 c.drawString(10, 800, 'ola mundo!')
+
+# linha
+x = 10
+y = h - 50
+c.line(x, y, x + 590, y)  # linha reta horizontal
+
+# mostra pagina
 c.showPage()
+
+# salvar o documento
 c.save()
