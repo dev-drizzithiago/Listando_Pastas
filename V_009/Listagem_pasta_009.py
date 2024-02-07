d1d104f74f03439ee49567be7c5b21197583e77c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-07 10:47:01 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -611,9 +611,12 @@ class ListandoArquivos:
         for valor_lista_busca in self.lista_analise_arq_busca:
             divisao_valor_extensao = str(valor_lista_busca).split('.')
             divisao_valor_pastas = str(valor_lista_busca).split('\\')
+
             valor_extensao = str(divisao_valor_extensao[-1]).lower().strip()
             ultima_pasta = str(divisao_valor_pastas[-2]).strip().lower()
+
             lista_analise_extensoes.append(valor_extensao)
+            
             print(f'{ultima_pasta} - {valor_extensao}')
         # del self.lista_analise_arq_busca[:]
 
