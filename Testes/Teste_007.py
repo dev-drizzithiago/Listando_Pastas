1aa1a7870f935d81775c71d5b463e121809cb293
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-07 09:14:16 -0300

    Update Teste_007.py

--- Testes/Teste_007.py
+++ Testes/Teste_007.py
@@ -1,6 +1,8 @@
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 
+w, h = A4
 c = canvas.Canvas('ola mundo.pdf', pagesize=A4)
 c.drawString(10, 800, 'ola mundo!')
+c.showPage()
 c.save()
