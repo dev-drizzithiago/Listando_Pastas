bfd6ce7b7d13879793ac96482cc251cf239e7dc8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-07 21:35:38 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -628,12 +628,12 @@ class ListandoArquivos:
                 chave_secundaria = valor_extensao
                 if valor_extensao == valor_busca_anterior:
                     dict_busca_ext[f'{chave_primaria}'].extend([f'{valor_extensao}'])
-                elif valor_extensao != valor_busca_anterior:
-                    dict_busca_ext = {f'{chave_secundaria}': f'{valor_extensao}'}
+                else:
+                    dict_busca_ext_nova = {**dict_busca_ext, **{f'{chave_secundaria}': f'{valor_extensao}'}}
 
             indice_busca += 1
 
-        print(dict_busca_ext)
+        print(dict_busca_ext_nova)
         # del self.lista_analise_arq_busca[:]
 
     def criando_arq_pdf(self):
