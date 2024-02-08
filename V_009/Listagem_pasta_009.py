7fb5beaa87ab4b8a80606cc447dfd5b2db4b4598
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-08 13:10:11 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -628,7 +628,9 @@ class ListandoArquivos:
                 lista_extensoes_encontradas.append(f'{indice_busca} - {valor_extensao}')
                 cont_encontrou += 1
             else:
-                extensao_primaria = valor_extensao
+                if valor_extensao != extensao_primaria:
+                    extensao_primaria = valor_extensao
+
                 if valor_extensao == extensao_primaria:
                     lista_extensoes_encontradas.append(f'{indice_busca} - {valor_extensao} | {cont_encontrou}')
                     cont_encontrou += 1
