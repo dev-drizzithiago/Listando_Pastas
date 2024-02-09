936929c47e7cb99b965bcb09c4372309973842d5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-09 20:23:50 -0300

    Update Teste_008.py

--- Testes/Teste_008.py
+++ Testes/Teste_008.py
@@ -1,2 +1,9 @@
 
 lista_extenoes_valores = ['jpg', 'png', 'mp4', 'mp3', 'pfd', 'txt', 'dll', 'exe', 'ini']
+
+valor_extensao = input('Digite uma extensao: ').lower().strip()
+
+if valor_extensao in lista_extenoes_valores:
+    print(True)
+else:
+    print(False)
