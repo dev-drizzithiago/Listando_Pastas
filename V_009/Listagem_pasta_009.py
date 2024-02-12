dcce2bbc0f979250d4da8c8ba9a4da49b6bb2c6c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-12 16:47:51 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -43,8 +43,8 @@ class ListandoArquivos:
         # Janela Principal
         self.janela_principal = tk.Tk()
         self.janela_principal.title('Versão 009')
-        self.janela_principal.geometry('1000x685+150+100')
-        self.janela_principal.config(pady=20, padx=20)
+        self.janela_principal.geometry('1000x550+150+100')
+        self.janela_principal.config(pady=1, padx=1)
 
         self.icone_busca = tk.PhotoImage(file='lupa.png')
         self.janela_principal.iconphoto(True, self.icone_busca)
