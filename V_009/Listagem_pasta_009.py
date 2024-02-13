ec0165c2367d3c50340effdd219b3fa36c125420
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-12 21:24:18 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -179,20 +179,21 @@ class ListandoArquivos:
         self.botao_adicionar_extensao.config(width=30)
         self.botao_adicionar_extensao.config(state=tk.DISABLED)
         self.botao_adicionar_extensao.config(command=self.janela_add_ext_arq_txt)
-        self.botao_adicionar_extensao.pack(anchor='w')
+        self.botao_adicionar_extensao.pack()
+        self.botao_adicionar_extensao.place(y=30)
 
         # BOTÃO DESTINO DA BUSCA
         self.botao_destino_busca = Button(self.label_frame_botoes_opcoes, text='Selecionar Pasta para Busca')
         self.botao_destino_busca.config(width=30)
         self.botao_destino_busca.config(command=self.thread_selecionar_destino_busca)
-        self.botao_destino_busca.place(y=1, x=790)
+        self.botao_destino_busca.place(y=1, x=788)
 
         # BOTÃO SAVE BUSCA
         self.botao_save_busca = Button(self.label_frame_botoes_opcoes, text='Salvar Busca')
         self.botao_save_busca.config(width=30)
         self.botao_save_busca.config(state=tk.DISABLED)
         self.botao_save_busca.config(command=lambda: self.salvando_resultado())
-        self.botao_save_busca.place(y=30, x=790)
+        self.botao_save_busca.place(y=30, x=788)
         # self.botao_save_busca.pack(anchor='e')
 
         # _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
