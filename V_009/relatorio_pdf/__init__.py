83ee4ffbe06f2278158d857abf967456a075c539
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-14 12:56:05 -0300

    Update __init__.py

--- V_009/relatorio_pdf/__init__.py
+++ V_009/relatorio_pdf/__init__.py
@@ -1,7 +1,7 @@
 from reportlab.pdfgen import canvas
 
 
-def criando_relatorio_pdf(relatorio, data, hora):
+def criando_relatorio_pdf(relatorio, pasta_destino, data, hora):
     print(relatorio, data, hora)
     relatorio_pdf = canvas.Canvas(f'Relatorio-{data}-{hora}')
     relatorio_pdf.drawString(10, 200, "Relatorios")
