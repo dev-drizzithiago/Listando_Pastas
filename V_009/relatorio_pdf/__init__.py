02dd4b8602ca007d082e56c09af5a4193e1a86fb
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-14 13:16:07 -0300

    Delete __init__.py

--- V_009/relatorio_pdf/__init__.py
+++
@@ -1,8 +0,0 @@
-from reportlab.pdfgen import canvas
-
-
-def criando_relatorio_pdf(relatorio, pasta_destino, data, hora):
-    print(relatorio, pasta_destino, data, hora)
-    relatorio_pdf = canvas.Canvas(f'Relatorio-{data}-{hora}.pdf')
-    relatorio_pdf.drawString(10, 200, "Relatorios")
-    relatorio_pdf.save()
