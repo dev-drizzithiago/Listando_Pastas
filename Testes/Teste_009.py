2d80d8e2580828acb803ac21acf2eaf5dad89f6f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-15 11:06:48 -0300

    Update Teste_009.py

--- Testes/Teste_009.py
+++ Testes/Teste_009.py
@@ -21,18 +21,19 @@ x_linha = 50
 y_linha = h - 50
 
 # DECLARAÇÃO TEXTO
-x_txt = 45
-y_txt = h - 45
+x_txt = 50
+y_txt = h - 70
 
 # CORPO RELATORIO
 arquivo_pdf = str('Relatorio_' + data + '_' + hora + '.pdf')
 
 relatorio_pdf = canvas.Canvas('arquivo_pdf.pdf', pagesize=A4)
 relatorio_pdf.drawString(420, 800, f"Relatorio {data_atual}")
-relatorio_pdf.line(x_linha, y_linha, x_linha + 450, y_linha)
+relatorio_pdf.line(x_linha, y_linha, x_linha + 500, y_linha)  # Primeira linha
+relatorio_pdf.line(x_linha, y_linha - 730, x_linha + 500, y_linha - 730)  # Última linha
 
 texto = relatorio_pdf.beginText(x_txt, y_txt)
-for cont in range(100):
+for cont in range(1, 51):
     texto.textLines(f'{cont}')
 relatorio_pdf.drawText(texto)
 
