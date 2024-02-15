e636aabb1524a2825d58b3a15054aa9b5bc65db5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-15 10:18:04 -0300

    Update Teste_009.py

--- Testes/Teste_009.py
+++ Testes/Teste_009.py
@@ -21,8 +21,8 @@ x_linha = 50
 y_linha = h - 50
 
 # DECLARAÇÃO TEXTO
-x_txt = 25
-y_txt = h - 25
+x_txt = 52
+y_txt = h - 50
 
 # CORPO RELATORIO
 arquivo_pdf = str('Relatorio_' + data + '_' + hora + '.pdf')
@@ -30,7 +30,10 @@ arquivo_pdf = str('Relatorio_' + data + '_' + hora + '.pdf')
 relatorio_pdf = canvas.Canvas('arquivo_pdf.pdf', pagesize=A4)
 relatorio_pdf.line(x_linha, y_linha, x_linha + 500, y_linha)
 
-texto = relatorio_pdf.beginText()
+texto = relatorio_pdf.beginText(x_txt, y_txt + 500)
+texto.setFont('Helvetica', 12)
+texto.textLine('Testando')
+relatorio_pdf.drawText(texto)
 
-relatorio_pdf.drawString(x + 380, 800, f"Relatorio {data_atual}")
+relatorio_pdf.drawString(420, 800, f"Relatorio {data_atual}")
 relatorio_pdf.save()
