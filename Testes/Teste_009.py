011ca4168849a24c19ba82016ceba899af5969ce
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-15 09:52:35 -0300

    Update Teste_009.py

--- Testes/Teste_009.py
+++ Testes/Teste_009.py
@@ -15,12 +15,13 @@ hora_atual = valor_datatime.strftime('%H:%M')
 data = data_atual.replace('/', '')
 hora = hora_atual.replace(':', '')
 
+w, h = A4
 x = 50
 y = h - 50
 
 # CORPO RELATORIO
 arquivo_pdf = str('Relatorio_' + data + '_' + hora + '.pdf')
-relatorio_pdf = canvas.Canvas(arquivo_pdf, pagesize=A4)
-relatorio_pdf.line(x, y, x + 200, y)
+relatorio_pdf = canvas.Canvas('arquivo_pdf.pdf', pagesize=A4)
+relatorio_pdf.line(x, y, x + 500, y)
 relatorio_pdf.drawString(10, 800, "Relatorios")
 relatorio_pdf.save()
