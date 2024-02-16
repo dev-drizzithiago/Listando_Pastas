f4526dfafa53c0d003b17c36c393cf02b6388d33
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-16 09:22:21 -0300

    Update Teste_011.py

---
+++ Testes/Teste_011.py
@@ -0,0 +1,21 @@
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import mm
+
+
+# from reportlab.pdfgen import canvas
+
+# ----------------------------------------------------------------------------------------------------------------------
+def addNumeroPaginas(canvas, doc):
+    """ Adicionando número de páginas """
+    num_pag = canvas.getPageNumber()
+    texto = "Pagina #%s" % num_pag
+    canvas.drawRightString(200*mm, 208*mm, texto)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+def criando_multi_paginas():
+    """Criando diversas paginas"""
+
