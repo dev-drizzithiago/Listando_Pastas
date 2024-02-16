cfc1cc17da3a39261dab50f7d022b0cdf721023f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-16 09:06:33 -0300

    Update Teste_010.py

--- Testes/Teste_010.py
+++ Testes/Teste_010.py
@@ -1,5 +1,18 @@
 from reportlab.pdfgen import canvas
 
+
 # ----------------------------------------------------------------------------------------------------------------------
 def criandoMultiPaginas():
-    dcoumento_pdf = canvas.Canvas('')
+    documento_pdf = canvas.Canvas('documento_teste.pdf')
+
+    for i in range(5):
+        num_pagina = documento_pdf.getPageNumber()
+        texto_pg = 'Está é a pagina %s' % num_pagina
+        documento_pdf.drawString(100, 750, texto_pg)
+        documento_pdf.showPage()
+    documento_pdf.save()
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    criandoMultiPaginas()
