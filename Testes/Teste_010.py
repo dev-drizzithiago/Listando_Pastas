f42f9dcb0c33546762e05d58e0ae7639ffd9308b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-16 09:13:25 -0300

    Update Teste_010.py

--- Testes/Teste_010.py
+++ Testes/Teste_010.py
@@ -5,7 +5,7 @@ from reportlab.pdfgen import canvas
 def criandoMultiPaginas():
     documento_pdf = canvas.Canvas('documento_teste.pdf')
 
-    for i in range(5):
+    for i in range(3):
         num_pagina = documento_pdf.getPageNumber()
         texto_pg = 'Está é a pagina %s' % num_pagina
         documento_pdf.drawString(100, 750, texto_pg)
