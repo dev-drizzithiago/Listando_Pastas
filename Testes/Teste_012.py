57a482ac1eaa7a4ec1408f3cf1191285a33e35fd
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-16 11:17:45 -0300

    Update Teste_012.py

--- Testes/Teste_012.py
+++ Testes/Teste_012.py
@@ -4,3 +4,10 @@ from reportlab.pdfgen import canvas
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import mm
+
+
+class PageNumCanvas(canvas.Canvas):
+    """
+    http://code.activestate.com/recipes/546511-page-x-of-y-with-reportlab/
+    http://code.activestate.com/recipes/576832/
+    """
