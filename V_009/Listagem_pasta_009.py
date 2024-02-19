c582befd3804d3920327f4397423c926e5069a62
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-19 09:37:50 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -663,16 +663,19 @@ class ListandoArquivos:
             divisao_valor_pastas = str(valor_lista_busca).split('\\')
             valor_extensao = str(divisao_valor_extensao[-1]).lower().strip()
             valor_pastas = str(divisao_valor_pastas[-2].lower()).strip()
-            print(valor_pastas)
-            if valor_pastas in self.contagem_pastas:
-                self.contagem_pastas[valor_pastas] += 1
-            else:
-                self.contagem_pastas[valor_pastas] = 1
 
             if valor_extensao in self.contagem_extensao:
                 self.contagem_extensao[valor_extensao] += 1
             else:
                 self.contagem_extensao[valor_extensao] = 1
+
+        for valor_pastas in divisao_valor_pastas:
+            print(valor_pastas)
+            '''if valor_pastas in self.contagem_pastas:
+                self.contagem_pastas[valor_pastas] += 1
+            else:
+                self.contagem_pastas[valor_pastas] = 1'''
+
         print('-=-' * 40)
         for extensao, quantidade in self.contagem_extensao.items():
             print('Tipos de extensão - Quantidade Arquivos')
