12b272331467ca6d871e6e5409f26a0bc09774ba
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-19 09:22:01 -0300

    Update Teste_012.py

--- Testes/Teste_012.py
+++ Testes/Teste_012.py
@@ -1,7 +1,7 @@
 from reportlab.lib.enums import TA_JUSTIFY
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfgen import canvas
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import mm
 
