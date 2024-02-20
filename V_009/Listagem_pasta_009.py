d7b2c2e7bfc80a29f42a450b8658a49c5a031bab
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-19 21:03:35 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -674,7 +674,8 @@ class ListandoArquivos:
                     extensao_encontradas[valor_busca] += 1
                 else:
                     extensao_encontradas[valor_busca] = 1
-
+                    
+        self.lista_result_busca.insert('end', '-=-' * 50)
         self.lista_result_busca.insert('end', f'{data_atual} - {hora_atual}')
         for chave, valor_encontrado in pastas_encontradas.items():
             self.lista_result_busca.insert('end', chave)
