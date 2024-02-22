5d47d76c084423f97458d10fa6edf7940f24966e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-22 10:01:59 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -761,40 +761,7 @@ class ListandoArquivos:
         del self.lista_analise_arq_busca[:]
 
     def criando_relatorio_pdf(self):
-        # Chamandos os modulos
-        from reportlab.pdfgen import canvas
-        from reportlab.lib.pagesizes import A4
-
-        # Declaração de variaveis
-        data = data_atual.replace('/', '')
-        hora = hora_atual.replace(':', '')
-        contador = 1
-        w, h = A4
-
-        # Declaração de linha
-        x_linha = 50
-        y_linha = h - 50
-
-        # DECLARAÇÃO TEXTO
-        x_txt = 50
-        y_txt = h - 70
-
-        # CORPO RELATORIO
-        arquivo_pdf = str('Relatorio_' + data + '_' + hora + '.pdf')
-
-        relatorio_pdf = canvas.Canvas('arquivo_pdf.pdf', pagesize=A4)
-        relatorio_pdf.drawCentredString(300, 800, f"Relatorio {data_atual}")
-        relatorio_pdf.line(x_linha, y_linha, x_linha + 500, y_linha)  # Primeira linha
-        relatorio_pdf.line(x_linha, y_linha - 730, x_linha + 500, y_linha - 730)  # Última linha
-
-        texto_indice = relatorio_pdf.beginText(x_txt, y_txt)
-        texto_string = relatorio_pdf.beginText(x_txt + 15, y_txt)
-
-        texto_string.textLines(self.lista_analise_arq_busca)
-        relatorio_pdf.drawText(texto_indice)
-        relatorio_pdf.drawText(texto_string)
-
-        relatorio_pdf.save()
+        pass
 
     def salvando_resultado(self):
         tipo_de_arquivo = [('Texto(.log)', '*.log')]
