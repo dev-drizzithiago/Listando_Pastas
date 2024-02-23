334867cccbe06ef7a6b0b7b5579dc8e70782cab4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-23 20:15:53 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -776,7 +776,7 @@ class ListandoArquivos:
                 print(f'[{valor_da_busca}]')
                 self.linha_aparencia()
                 valor_pasta = Path(valor_da_busca)
-
+                for valo_subpasta in valor_pasta.glob(valo_subpasta)
 
     def analise_dados_busca(self):
         self.criando_relatorio_pdf()
