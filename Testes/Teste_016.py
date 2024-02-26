4f9013267e062a10c30dc66816c7dc287cb59e37
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-26 20:47:51 -0300

    Update Teste_016.py

--- Testes/Teste_016.py
+++ Testes/Teste_016.py
@@ -1,7 +1,14 @@
 from time import sleep
 from progress.bar import Bar
 
+from tqdm import tqdm
+
 with Bar('Lendo...', max=100) as bar:
     for i in range(100):
         sleep(0.02)
         bar.next()
+
+for i in tqdm(range(101), desc='Lendo...', ascii=False, ncols=75):
+    sleep(0.01)
+
+print('completo..!')
