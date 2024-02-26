09f68b73c4c45aae448262e72a9ebc22a7fafcab
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-02-26 20:56:42 -0300

    Update Teste_016.py

--- Testes/Teste_016.py
+++ Testes/Teste_016.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 with Bar('Lendo...', max=100) as bar:
     for i in range(100):
         sleep(0.02)
+        bar.start()
         bar.next()
 
 for i in tqdm(range(101), desc='Lendo...', ascii=False, ncols=75):
