5e5a837a3d495c9f5b9eb06a227c25ddebb44c1e
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-06 11:09:04 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -831,6 +831,7 @@ class ListandoArquivos:
                     self.lista_busca_arquivos.append(f'{caminho_files}')
                     itens_bytes = stat(caminho_files).st_size
                     if search(self.extensao_selecao_busca, valor_itens):
+                        self.status_arquivos.config(text=valor_itens)
                         self.lista_result_busca.insert('end', f'{destaque_arquivos_pasta}')
                         self.lista_analise_arq_busca.append(f'{caminho_files}')
                         self.status_contagem_arquivos.config(text=f'Arquivos encontrados: [{contador_arquivos}]')
