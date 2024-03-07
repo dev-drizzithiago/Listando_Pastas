0701e4d888f880551fa6e373ac54978a8bcdb9d6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-07 20:31:52 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -158,7 +158,7 @@ class ListandoArquivos:
         self.status_contagem_arquivos = Label(self.label_frame_geral_info)
         self.status_contagem_arquivos.config(text=self.var_status_contagem_arquivos.get())
         self.status_contagem_arquivos.config(justify='center')
-        self.status_contagem_arquivos.pack(side='left', pady=2, padx=2)
+        self.status_contagem_arquivos.pack(side='bottom', pady=2, padx=2)
 
         # LABEL PASTAS DISTINO DA BUSCA
         self.var_status_contagem_pastas = tk.StringVar()
@@ -175,7 +175,7 @@ class ListandoArquivos:
         self.status_arquivos = Label(self.label_frame_geral_info, text=self.var_msg_tot_busca.get())
         self.status_arquivos.config(justify='center')
         self.status_arquivos.pack(anchor='n', pady=2, padx=2)
-        self.status_arquivos.place(y=2, x=400)
+        self.status_arquivos.place(y=2, x=10)
 
         # LABEL PASTA PRINCIPAL DE BUSCA
         # self.var_label_pasta_principal = tk.StringVar()
