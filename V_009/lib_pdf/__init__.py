c750a156f76ad04d6c23d237be5ef153957e6faf
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-13 15:21:17 -0300

    Update __init__.py

--- V_009/lib_pdf/__init__.py
+++ V_009/lib_pdf/__init__.py
@@ -9,42 +9,41 @@ from tkinter.simpledialog import askstring
 from tkinter.messagebox import showinfo
 from pathlib import Path
 
-"""Escolhendo as informações para salvar o arquivo"""
-showinfo('AVISA!', 'Escolha o direto para salvar o documento')
-local_save = str(Path(askdirectory()))
-nome_arquivo_pdf = askstring('Imprestante!', 'Digite o nome do arquivo')
-pdf_diretorio_save = str(local_save + '\\' + nome_arquivo_pdf + '.pdf')
-print(pdf_diretorio_save)
 
-dados_da_busca = 
+def criando_documento_pdf(dados_coletados=''):
+    """Escolhendo as informações para salvar o arquivo"""
+    showinfo('AVISA!', 'Escolha o direto para salvar o documento')
+    local_save = str(Path(askdirectory()))
+    nome_arquivo_pdf = askstring('Imprestante!', 'Digite o nome do arquivo')
+    pdf_diretorio_save = str(local_save + '\\' + nome_arquivo_pdf + '.pdf')
+    print(pdf_diretorio_save)
 
-""" Criando o Arquivos PDF"""
-lista_teste = ['Thiago', 'Zenny', 'Enzo']
+    dados_da_busca = dados_coletados
 
+    """ Criando o Arquivos PDF"""
+    lista_teste = ['Thiago', 'Zenny', 'Enzo']
 
-def numero_paginas(janela, documento):
-    """Adicionao número de paginas"""
-    num_pag = janela.getPageNumber()
-    pagina = f'Pagina {num_pag}'
-    janela.drawRightString(200 * mm, 20 * mm, pagina)
+    def numero_paginas(janela, documento):
+        """Adicionao número de paginas"""
+        num_pag = janela.getPageNumber()
+        pagina = f'Pagina {num_pag}'
+        janela.drawRightString(200 * mm, 20 * mm, pagina)
 
+    def documento_PDF():
+        """Salvando as informações no documento"""
+        doc = SimpleDocTemplate(dados_da_busca, pagezsize=A4, rightMargin=72, leftMargin=72,
+                                topMargin=72, bottomMargin=18)
+        estilo = getSampleStyleSheet()
+        estilo.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
 
-def documento_PDF():
-    """Salvando as informações no documento"""
-    doc = SimpleDocTemplate(pdf_diretorio_save, pagezsize=A4, rightMargin=72, leftMargin=72,
-                            topMargin=72, bottomMargin=18)
-    estilo = getSampleStyleSheet()
-    estilo.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+        dados_save = []
 
-    dados_save = []
+        for dados in lista_teste:
+            texto = f'<font size="12">%s</font>' % dados
+            dados_save.append(Paragraph(texto, estilo["Justify"]))
+            dados_save.append(Spacer(1, 10))
 
-    for dados in lista_teste:
-        texto = f'<font size="12">%s</font>' % dados
-        dados_save.append(Paragraph(texto, estilo["Justify"]))
-        dados_save.append(Spacer(1, 10))
+        doc.build(dados_save, onFirstPage=numero_paginas, onLaterPages=numero_paginas)
 
-    doc.build(dados_save, onFirstPage=numero_paginas, onLaterPages=numero_paginas)
-
-
-if __name__ == '__main__':
-    documento_PDF()
+    if __name__ == '__main__':
+        documento_PDF()
