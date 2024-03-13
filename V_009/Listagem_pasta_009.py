b978c9edf0ec670f5dda595e5e86fc84b0f8019b
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-13 18:41:25 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -6,10 +6,9 @@ from time import sleep
 from pathlib import Path
 from threading import Thread
 from datetime import datetime
-from tkinter.messagebox import showerror
+from tkinter.messagebox import showerror, showinfo
 from tkinter.simpledialog import askstring
 from tkinter.filedialog import askdirectory, asksaveasfile
-from lib_pdf import criando_documento_pdf
 
 valor_pasta_destino = Path().home()
 pasta_arq_registro_extensao = str(Path(valor_pasta_destino, 'AppData', 'LocalLow', 'extensoes'))
@@ -799,11 +798,53 @@ class ListandoArquivos:
         self.label_status.config(text='Analise finalizada!!')
 
     def criando_relatorio_pdf(self):
+        from reportlab.lib.enums import TA_JUSTIFY
+        from reportlab.lib.pagesizes import A4
+        from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak
+        from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+        from reportlab.lib.units import mm
+        
         teste_1 = 'thiago'
         teste_2 = 'zenilda'
         teste_3 = 'enzo'
-        # criando_documento_pdf(teste_1, teste_2, teste_3)
-        criando_documento_pdf(self.lista_result_busca, self.lista_qtd_extensao, self.lista_qtd_arq_pastas)
+        
+        """Escolhendo as informações para salvar o arquivo"""
+        showinfo('AVISA!', 'Escolha o direto para salvar o documento')
+        local_save = str(Path(askdirectory()))
+        nome_arquivo_pdf = askstring('Imprestante!', 'Digite o nome do arquivo')
+        pdf_diretorio_save = str(local_save + '\\' + nome_arquivo_pdf + '.pdf')
+        print(pdf_diretorio_save)
+
+
+        """ Criando o Arquivos PDF"""
+        lista_teste = ['Thiago', 'Zenny', 'Enzo']
+
+        # ----------------------------------------------------------------------
+        def numero_paginas(janela, documento):
+            """Adicionao número de paginas"""
+            num_pag = janela.getPageNumber()
+            pagina = f'Pagina {num_pag}'
+            janela.drawRightString(200 * mm, 20 * mm, pagina)
+
+        # ----------------------------------------------------------------------
+        def documento_PDF():
+            """Salvando as informações no documento"""
+            doc = SimpleDocTemplate(pdf_diretorio_save, pagezsize=A4, rightMargin=72, leftMargin=72,
+                                    topMargin=72, bottomMargin=18)
+            estilo = getSampleStyleSheet()
+            estilo.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+
+            dados_save = []
+
+            for dados in dados_da_busca:
+                texto = f'<font size="12">%s</font>' % dados
+                dados_save.append(Paragraph(texto, estilo["Justify"]))
+                dados_save.append(Spacer(1, 10))
+
+            doc.build(dados_save, onFirstPage=numero_paginas, onLaterPages=numero_paginas)
+
+        if __name__ == '__main__':
+            documento_PDF()
 
     def salvando_resultado(self):
         tipo_de_arquivo = [('Texto(.txt)', '*.txt')]
