765ac24bee30e20831c8123b2b41bb30ec5c6c79
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-13 09:59:30 -0300

    Update __init__.py

---
+++ V_009/lib_pdf/__init__.py
@@ -0,0 +1,8 @@
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from tkinter.filedialog import askdirectory
+from tkinter.simpledialog import askstring
+
+local_save = askdirectory()
+nome_arquivo_pdf = askstring('Imprestante!', 'Digite o nome do arquivo')
+pdf = canvas.Canvas(nome_arquivo_pdf + '.pdf', pagesize=A4)
