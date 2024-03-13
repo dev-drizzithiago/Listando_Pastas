d936f0b911824bab0886f903e716d1395c5f005f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-13 10:19:51 -0300

    Update __init__.py

--- V_009/lib_pdf/__init__.py
+++ V_009/lib_pdf/__init__.py
@@ -3,12 +3,15 @@ from reportlab.lib.pagesizes import A4
 from tkinter.filedialog import askdirectory
 from tkinter.simpledialog import askstring
 from tkinter.messagebox import showinfo
+from pathlib import Path
 
 
 """Escolhendo as informações para salvar o arquivo"""
 showinfo('AVISA!', 'Escolha o direto para salvar o documento')
-local_save = askdirectory()
+local_save = Path(askdirectory())
 nome_arquivo_pdf = askstring('Imprestante!', 'Digite o nome do arquivo')
-
+pdf_diretorio_save = local_save + '\\' + nome_arquivo_pdf + '.pdf'
+print(pdf_diretorio_save)
 """ Criando o Arquivos PDF"""
-pdf = canvas.Canvas(nome_arquivo_pdf + '.pdf', pagesize=A4)
+pdf = canvas.Canvas(pdf_diretorio_save, pagesize=A4)
+pdf.save()
