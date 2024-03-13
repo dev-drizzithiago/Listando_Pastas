10302caedf92e85d333300599db20aa0acef2f67
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-13 10:05:37 -0300

    Update __init__.py

--- V_009/lib_pdf/__init__.py
+++ V_009/lib_pdf/__init__.py
@@ -4,7 +4,11 @@ from tkinter.filedialog import askdirectory
 from tkinter.simpledialog import askstring
 from tkinter.messagebox import showinfo
 
+
+"""Escolhendo as informações para salvar o arquivo"""
 showinfo('AVISA!', 'Escolha o direto para salvar o documento')
 local_save = askdirectory()
 nome_arquivo_pdf = askstring('Imprestante!', 'Digite o nome do arquivo')
+
+""" Criando o Arquivos PDF"""
 pdf = canvas.Canvas(nome_arquivo_pdf + '.pdf', pagesize=A4)
