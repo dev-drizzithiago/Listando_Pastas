2a1b701033e7f78e29b95f45dfb695dbb3ea9909
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-18 21:07:16 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -796,7 +796,6 @@ class ListandoArquivos:
         self.lista_result_busca.insert('end', '')
         self.lista_result_busca.insert('end', 'Analise finalizada!!')
         self.label_status.config(text='Analise finalizada!!')
-        self.criando_relatorio_pdf()
 
     def criando_relatorio_pdf(self):
         valor_nome_PDF = askstring('AVISO!', 'Dê um nome ao arquivo PDF')
@@ -806,6 +805,9 @@ class ListandoArquivos:
         documento_PDF(self.lista_save_busca, nome_PDF)
 
     def salvando_resultado(self):
+
+        self.criando_relatorio_pdf()
+
         tipo_de_arquivo = [('Texto(.txt)', '*.txt')]
         arquivo_save = asksaveasfile(filetypes=tipo_de_arquivo, defaultextension=tipo_de_arquivo)
         try:
