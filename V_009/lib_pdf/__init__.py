2f8dbf461f0d5db4a34ad4cd30d022132304c2ca
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-19 11:49:16 -0300

    Update __init__.py

--- V_009/lib_pdf/__init__.py
+++ V_009/lib_pdf/__init__.py
@@ -1,9 +1,10 @@
 from reportlab.lib.enums import TA_JUSTIFY
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image, PageBreak
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import mm
 from pathlib import Path
+from tkinter.messagebox import showinfo, showerror, showwarning
 
 home = Path.home()
 diretorio_arquivo_save = str(Path(home, 'Downloads'))
@@ -28,24 +29,27 @@ def documento_PDF(valor_dados_coletados='<desconhecido>', nome_documento='descon
     """Nas duas linhas abaixo, vai mostrar os testes estão tudo correto"""
     for valor in valor_dados_coletados:
         print(valor)
+    try:
+        """Salvando as informações no documento"""
+        doc = SimpleDocTemplate(pdf_diretorio_save, pagezsize=A4, rightMargin=72, leftMargin=72,
+                                topMargin=72, bottomMargin=18)
+        estilo = getSampleStyleSheet()
+        estilo.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
 
-    """Salvando as informações no documento"""
-    doc = SimpleDocTemplate(pdf_diretorio_save, pagezsize=A4, rightMargin=72, leftMargin=72,
-                            topMargin=72, bottomMargin=18)
-    estilo = getSampleStyleSheet()
-    estilo.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+        dados_save = []
 
-    dados_save = []
+        for dados in valor_dados_coletados:
+            texto = f'<font size="12">%s</font>' % dados
+            dados_save.append(Paragraph(texto, estilo["Justify"]))
+            dados_save.append(Spacer(1, 10))
 
-    for dados in valor_dados_coletados:
-        texto = f'<font size="12">%s</font>' % dados
-        dados_save.append(Paragraph(texto, estilo["Justify"]))
-        dados_save.append(Spacer(1, 10))
+        doc.build(dados_save, onFirstPage=numero_paginas, onLaterPages=numero_paginas)
 
-    doc.build(dados_save, onFirstPage=numero_paginas, onLaterPages=numero_paginas)
-
-    print('Finalizado!')
-
+        showinfo('Parabens!', f'O documento foi salvo com sucesso na pasta {'Downloads'}')
+        print('\nFinalizado!')
+    except:
+        print(f'ERROR: Não foi possível gravar o documento {pdf_diretorio_save}')
+        showerror("ERROR", f'Não foi possível gravar o documento {pdf_diretorio_save}')
 
 if __name__ == '__main__':
     documento_PDF()
