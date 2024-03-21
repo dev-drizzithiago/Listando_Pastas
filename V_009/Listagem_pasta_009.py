bbcd7f67ef25ecf3ebe547311740924f351860ee
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-21 20:54:06 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -829,7 +829,7 @@ class ListandoArquivos:
 
     def save_PDF(self):
         valor_nome_PDF = askstring('AVISO!', 'Dê um nome ao arquivo PDF')
-        nome_PDF = f'{valor_nome_PDF}-{data_atual.replace("/", "")}-{hora_atual.replace(':', '')}'
+        nome_PDF = f'{valor_nome_PDF}-{data_atual.replace("/", "")}-{hora_atual.replace(":", "")}'
         sleep(1)
         print(nome_PDF)
         documento_PDF(self.lista_save_busca, nome_PDF, self.lista_qtd_extensao, self.lista_qtd_arq_pastas)
