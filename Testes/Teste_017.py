c29504fca073cad74e5685713993f9eadc986d81
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-24 20:05:55 -0300

    Update Teste_017.py

--- Testes/Teste_017.py
+++ Testes/Teste_017.py
@@ -1,7 +1,7 @@
-import matplotlib.pyplot
+from matplotlib.pyplot import plot, show, title
 
 meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho']
 valores = [9, 5, 10, 2, 0, 8]
 
-matplotlib.pyplot.plot(meses, valores)
-matplotlib.pyplot.show()
+plot(meses, valores)
+show()
