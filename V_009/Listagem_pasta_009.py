0412b1a85616bf1f7fc81b009c5cb9477db464e4
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-24 20:17:43 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -814,7 +814,7 @@ class ListandoArquivos:
 
         """# Adiciona na lista de busca a quantidade de extensões que foram encontradas no processo de busca."""
         for extensao, quantidade in self.contagem_extensao.items():
-            valor_extensao_qtd = f' {extensao.upper()} {"":=^17} {quantidade}'
+            valor_extensao_qtd = f' {extensao.upper()} - {quantidade}'
             self.lista_final_busca.append(valor_extensao_qtd)
             print(f'{valor_extensao_qtd}')
             self.lista_qtd_extensao.append(valor_extensao_qtd)
@@ -834,7 +834,7 @@ class ListandoArquivos:
 
         """# Adiciona na lista de busca, os contagem dos itens encontrados nas pastas"""
         for pastas, quantidade in self.contagem_pastas.items():
-            qtd_arq_pastas = f'\\{pastas.upper()} {"":=^17} [{quantidade}'
+            qtd_arq_pastas = f'\\{pastas.upper()} - {quantidade}'
             print(f'{qtd_arq_pastas}')
             self.lista_qtd_arq_pastas.append(qtd_arq_pastas)
             self.lista_result_busca.insert('end', qtd_arq_pastas)
