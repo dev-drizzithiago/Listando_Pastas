90df1ec1fc11d60e6b397952f699b997d6498aa5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-03-24 19:52:02 -0300

    Update Teste_017.py

--- Testes/Teste_017.py
+++ Testes/Teste_017.py
@@ -1,3 +1,7 @@
 import matplotlib.pyplot
 
-meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', ]
+meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+         'Julho', 'Agosto', 'Setembro', "outubro", 'Novembro', 'Desembro']
+valores = [105235, 107697, 110256, 109236, 108859, 109986]
+
+matplotlib.pyplot(meses, valores)
