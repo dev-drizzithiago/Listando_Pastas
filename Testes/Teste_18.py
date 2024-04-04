8d1be1797334e371671587d82e04884126e23815
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-04 20:30:22 -0300

    Create Teste_18.py

---
+++ Testes/Teste_18.py
@@ -0,0 +1,20 @@
+
+valor_ext_grafico = ['txt=10', 'pdf=7','log=1',
+                     'png=2', 'jpg=9', 'zip=1',
+                     'rar=1', 'mp4=11', 'jpeg=1']
+
+"""# Declaração Variaveis"""
+extensao = list()
+quantidade = list()
+valor_quantidade_int = list()
+
+dict_valores_graficos = {'Extensao': None, 'Quantidade': None}
+
+"""# Lendos arquivo 'valor_ext_grafico"""
+for dividindo_valores in valor_ext_grafico:
+    extensao.append(str(dividindo_valores.split('=')[0]).strip())
+    quantidade.append(str(dividindo_valores.split('=')[1]).strip())
+    dict_valores_graficos['Extensao'] = extensao
+    dict_valores_graficos['Quantidade'] = quantidade
+
+
