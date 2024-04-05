535b0648252eb17e75f42764c798bb691d700599
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-05 14:45:21 -0300

    Update __init__.py

--- V_009/lib_pdf/__init__.py
+++ V_009/lib_pdf/__init__.py
@@ -6,22 +6,14 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import mm
 from pathlib import Path
 
-"""# Modulo PANDAS"""
-import pandas as pd
-
 """# Modulos GERAL"""
 from tkinter.messagebox import showinfo, showerror
 
 home = Path.home()
 diretorio_arquivo_save = str(Path(home, 'Downloads'))
 
-"""# Modulo Grafico"""
-import matplotlib.pyplot as plt
-import numpy as np
-
 
 # _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
-
 
 # ----------------------------------------------------------------------
 def numero_paginas(janela, documento):
@@ -85,61 +77,10 @@ def documento_PDF(valor_dados_coletados=None, valor_nome_documento=None, valor_q
     print(type(quantidade), 'Quantidade')
     print(type(extensao), 'Extensão')
 
-    """# Apenas testes com pandas"""
-    df_1 = pd.Series(quantidade, index=extensao)
-    df_2 = pd.DataFrame(dict_valores_graficos)
 
-    print(f'\n{df_1}\n')
-    print(f'\n{df_2}\n')
 
-    """#### Teste grafico pizza"""
 
-    """# Criando a representação, plotagem"""
-    fig, ax = plt.subplots(figsize=(10, 5), subplot_kw=dict(aspect='equal'))
-
-    """# Abaixo a função vai ser responsavel ela porcentagem dos valores"""
-
-    def func(pct, allvals):
-        # Calc %
-        absoluto = int(pct / 100. * np.sum(allvals))
-        # Legendao do grafico com %
-        return "{:.1f}%\n({:d})".format(pct, absoluto)
-
-    """# Criando o grafico e colocand as legendas"""
-    wedges, textos, texto_auto = ax.pie(valor_quantidade_int, textprops=dict(color="w"),
-                                        autopct=lambda pct: func(pct, valor_quantidade_int))
-
-    """# Define a caixa de legenda externa, titulos, localização e onde vai ancorar o box"""
-    ax.legend(wedges, extensao, title='teste', loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
-
-    """# Define o tamanho do texto dentro do grafico """
-    plt.setp(texto_auto, size=6, weight='bold')
-
-    """# Titulo do grafico"""
-    ax.set_title('Extensões encontradas')
-
-    """# Rodando o grafico"""
-    plt.show()
-
-    """### Grafico barras"""
-    """# Montando o grafico em barras"""
-    plt.bar(extensao, valor_quantidade_int, color='red')
-
-    """# Define a legenda de cada barro, no exio X"""
-    plt.xticks(extensao)
-
-    """# A label para o eixo Y"""
-    plt.ylabel('Quantidade encontrada')
-
-    """# A label para o eixo X"""
-    plt.xlabel('Extensões')
-
-    """# Titulo do Grafico"""
-    plt.title('Extensões encontradas')
-
-    """# Rodando o grafico"""
-    plt.show()
-
+   
     """### Criando parametros para salvar o arquivo no diretorio 'DOWNLOADS' do windows. """
     nome_arquivo_pdf = str(valor_nome_documento)
     pdf_diretorio_save = diretorio_arquivo_save + "\\" + nome_arquivo_pdf + '.pdf'
