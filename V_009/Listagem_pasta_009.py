bd00e8f448386ae393dbd702d1226cc1e3155efa
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-06 22:53:45 -0300

    Update Listagem_pasta_009.py

--- V_009/Listagem_pasta_009.py
+++ V_009/Listagem_pasta_009.py
@@ -14,7 +14,7 @@ from tkinter.filedialog import askdirectory, asksaveasfile
 from lib_pdf import *
 
 """# Modulo para criar um grafico em diversos formatos"""
-from lib_grafico import *
+from lib_grafico import opcao_selecionada
 
 valor_pasta_destino = Path().home()
 pasta_arq_registro_extensao = str(Path(valor_pasta_destino, 'AppData', 'LocalLow', 'extensoes'))
@@ -223,7 +223,6 @@ class ListandoArquivos:
         # _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
         """#### Separador"""
         Separator(self.janela_principal, orient=tk.HORIZONTAL).place(y=590, x=1)
-
 
         # _+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+
         """#### LABEM FRAME INFO BUSCA"""
