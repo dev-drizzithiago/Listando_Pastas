fee445b4604a6c57ba684d3ca4395537dd376370
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-09 11:02:25 -0300

    Update Listando_pasta_010.py

--- V_010/Listando_pasta_010.py
+++ V_010/Listando_pasta_010.py
@@ -1,10 +1,19 @@
 """#### Declaração de Modulos"""
-import tkinter.ttk as ttk
+import tkinter.ttk
 import tkinter as tk
+
 
 class ProgramaPrincipal:
     def __init__(self):
+        """# Janela principal"""
         self.janela_principal = tk.Tk()
+        self.janela_principal.title('V_010')
+        self.janela_principal.geometry('1000x680+150+5')
+        self.janela_principal.resizable(0, 0)
+
+        """#### frames"""
+        self.frames_superior_01 = tk.Frame(self.janela_principal)
+        self.frames_superior_01.pack(anchor='n')
 
         self.janela_principal.mainloop()
 
