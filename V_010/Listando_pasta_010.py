9f0b3af5a31415f33efb4482ba079237989b34ba
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-09 10:33:17 -0300

    Update Listando_pasta_010.py

---
+++ V_010/Listando_pasta_010.py
@@ -0,0 +1,12 @@
+"""#### Declaração de Modulos"""
+import tkinter.ttk as ttk
+import tkinter as tk
+
+class ProgramaPrincipal:
+    def __init__(self):
+        self.janela_principal = tk.Tk()
+
+        self.janela_principal.mainloop()
+
+
+iniciando_obj = ProgramaPrincipal()
