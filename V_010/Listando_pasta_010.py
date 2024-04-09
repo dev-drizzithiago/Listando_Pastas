e60f78409850896d5d9a6b46f277ba632e8b495f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-09 15:19:00 -0300

    Update Listando_pasta_010.py

--- V_010/Listando_pasta_010.py
+++ V_010/Listando_pasta_010.py
@@ -2,6 +2,9 @@
 from tkinter.ttk import *
 import tkinter as tk
 
+"""# Modeulo THREAD"""
+from threading import Thread
+
 
 class ProgramaPrincipal:
     def __init__(self):
@@ -15,10 +18,10 @@ class ProgramaPrincipal:
         """#### LabelFrame Principal"""
         self.label_frame_principal = tk.LabelFrame(self.janela_principal)
         self.label_frame_principal.config(text='Bem vindo ao buscador de arquivos!')
-        self.label_frame_principal.config(bg='#696969')
+        self.label_frame_principal.config(bg='#DCDCDC')
         self.label_frame_principal.config(width=910, height=650)
         self.label_frame_principal.place(y=5, x=45)
-
+        # -=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
         """#### frames"""
         """# Frame SUPERIOR: Responsavel por mostrar as opções de buscas"""
         self.frames_superior = tk.Frame(self.janela_principal, bg='#A9A9A9')
@@ -26,17 +29,28 @@ class ProgramaPrincipal:
         self.frames_superior.config(bd=2)
         self.frames_superior.place(y=20, x=50)
 
+        """COMBO BOX"""
+        self.var_combo_box_categoria = tk.StringVar()
+        self.combo_box_cat = Combobox(self.frames_superior, textvariable=self.var_combo_box_categoria.get())
+        self.combo_box_cat.pack(anchor='center', fill='both')
+        self.combo_box_cat.place(y=1, x=10)
+
+        # -=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
         """# Frame Central: Reposanvel por mostrar o resultado da busca"""
         self.frames_central = tk.Frame(self.janela_principal, bg='#C0C0C0')
         self.frames_central.config(width=900, height=200)
         self.frames_central.config(bd=2)
         self.frames_central.place(y=230, x=50)
 
+        # -=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
         """# Frame Inferior: Responsável por registro das informações de busca"""
         self.frames_inferior = tk.Frame(self.janela_principal, bg='#D3D3D3')
         self.frames_inferior.config(width=900, height=200)
         self.frames_inferior.config(bd=2)
         self.frames_inferior.place(y=440, x=50)
+
+
+
 
         self.janela_principal.mainloop()
 
