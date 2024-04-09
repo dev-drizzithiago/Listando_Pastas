c7d051adb98f2f26f5edc0b83f1e621cb56082b1
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-09 11:09:11 -0300

    Update Listando_pasta_010.py

--- V_010/Listando_pasta_010.py
+++ V_010/Listando_pasta_010.py
@@ -12,8 +12,13 @@ class ProgramaPrincipal:
         self.janela_principal.resizable(0, 0)
 
         """#### frames"""
-        self.frames_superior_01 = tk.Frame(self.janela_principal)
-        self.frames_superior_01.pack(anchor='n')
+        self.frames_superior_01 = tk.Frame(self.janela_principal, bg='green')
+        self.frames_superior_01.config(width=900, height=200)
+        self.frames_superior_01.place(y=10, x=50)
+
+        self.frames_superior_02 = tk.Frame(self.janela_principal, bg='black')
+        self.frames_superior_02.config(width=900, height=200)
+        self.frames_superior_02.place(y=220, x=50)
 
         self.janela_principal.mainloop()
 
