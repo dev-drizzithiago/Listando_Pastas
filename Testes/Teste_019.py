73264a8bf8a1ab0f292529b47e9795e903e49bd0
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-10 22:36:35 -0300

    Update Teste_019.py

--- Testes/Teste_019.py
+++ Testes/Teste_019.py
@@ -1,10 +1,13 @@
 from tkinter import *
 
 tk = Tk()
+tk.geometry('400x400')
 
 var = []
 buttons = []
 k = 0
+colunas = 1
+linhas = 25
 
 AUDIO = ['teste1', 'teste2', 'teste3', 'teste4', 'teste5',
          'teste6', 'teste7', 'teste8', 'teste9', 'teste10', 'Final']
@@ -19,8 +22,14 @@ def delHist():
 for i in AUDIO:
     var.append(IntVar())
     buttons.append(Checkbutton(tk, text=i, variable=var[k], wraplength=500))
-    buttons[-1].pack()
+    buttons[-1].place(y=linhas, x=colunas)
     k += 1
+
+    if linhas == 110:
+        linhas = 25
+        colunas += 85
+    else:
+        linhas += 17
 
 Button(text='delHist', command=delHist).pack()
 
