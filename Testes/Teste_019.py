3ef4c6463d496ea871a7319244ccc0283794dc41
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-10 22:31:39 -0300

    Update Teste_019.py

--- Testes/Teste_019.py
+++ Testes/Teste_019.py
@@ -6,6 +6,9 @@ var = []
 buttons = []
 k = 0
 
+AUDIO = ['teste1', 'teste2', 'teste3', 'teste4', 'teste5',
+         'teste6', 'teste7', 'teste8', 'teste9', 'teste10', 'Final']
+
 
 def delHist():
     for i in range(len(var)):
@@ -13,10 +16,12 @@ def delHist():
             print(buttons[i]['text'])
 
 
-for i in 'abcde':
+for i in AUDIO:
     var.append(IntVar())
-    buttons.append(Checkbutton(tk, text=i[0], variable=var[k], wraplength=500))
+    buttons.append(Checkbutton(tk, text=i, variable=var[k], wraplength=500))
     buttons[-1].pack()
     k += 1
 
 Button(text='delHist', command=delHist).pack()
+
+tk.mainloop()
