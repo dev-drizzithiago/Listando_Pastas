5b63ca711e624f99ca37a51fe9c0c12a4164e6d7
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-15 22:07:02 -0300

    Update Listando_pasta_010.py

--- V_010/Listando_pasta_010.py
+++ V_010/Listando_pasta_010.py
@@ -121,20 +121,19 @@ class ProgramaPrincipal:
         self.var_lista_result_busca = tk.IntVar()
         self.lista_de_result_busca = tk.Listbox(self.frames_central_2, width=98, height=4, bg='#C0C0C0')
         self.lista_de_result_busca.config(font='Arial', justify='left', selectmode=tk.SINGLE)
-        self.lista_de_result_busca.place(y=115, x=5)
-        self.lista_de_result_busca.pack(side='bottom')
+        self.lista_de_result_busca.pack(anchor='s')
         # ______________________________________________________________________________________________________________
         """# Barra de Rolagem Y """
         self.barra_rolagem_lista_busca_y = tk.Scrollbar(self.lista_de_result_busca, orient=tk.VERTICAL)
         self.barra_rolagem_lista_busca_y.pack(fill=tk.Y)
-        self.barra_rolagem_lista_busca_y.place(y=13, x=864)
+        self.barra_rolagem_lista_busca_y.pack(side='right')
         self.barra_rolagem_lista_busca_y.config(command=self.lista_de_result_busca.yview)
         self.lista_de_result_busca.config(yscrollcommand=self.barra_rolagem_lista_busca_y.set)
         # ______________________________________________________________________________________________________________
         """# Barrade de Rolagem X """
         self.barra_rolagem_lista_busca_x = tk.Scrollbar(self.lista_de_result_busca, orient=tk.HORIZONTAL)
         self.barra_rolagem_lista_busca_x.pack(fill=tk.X)
-        self.barra_rolagem_lista_busca_x.place(y=58, x=400)
+        self.barra_rolagem_lista_busca_x.pack(side='bottom')
         self.barra_rolagem_lista_busca_x.config(command=self.lista_de_result_busca.xview)
         self.lista_de_result_busca.config(yscrollcommand=self.barra_rolagem_lista_busca_x.set)
         # ______________________________________________________________________________________________________________
