3fa71a655f4c612f4a17e348d436629db5a36c8f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-20 19:06:12 -0300

    Update Teste_020.py

--- Testes/Teste_020.py
+++ Testes/Teste_020.py
@@ -24,20 +24,19 @@ for valor in lista_da_busca:
 
     lista_dados.append(str(valor).split('\\')[-1].strip())
 
-    for valor_lista_dados in lista_dados:
-        print(valor_lista_dados)
+for valor_lista_dados in lista_dados:
+    print(valor_lista_dados)
 
+    for valor in range(0, 2, len(valor_lista_dados)):
+        if valor_lista_dados in duplicado:
+            duplicado[str(valor).split('\\')[-1].strip()] += 1
+        else:
+            duplicado[str(valor).split('\\')[-1].strip()] = 1
+    for k, v in duplicado.items():
+        if v > 1:
+            print(f'{k} - {v}')
 
-for valor in lista_dados:
-    if str(valor).split('\\')[-1].strip() in duplicado:
-        duplicado[str(valor).split('\\')[-1].strip()] += 1
-    else:
-        duplicado[str(valor).split('\\')[-1].strip()] = 1
-for k, v in duplicado.items():
-    if v > 1:
-        print(f'{k} - {v}')
-
-for i in range(0, len(lista_dados) - 1):
-    comparacao_1 = lista_dados[i]
-    if comparacao_1 == lista_dados[i + 1]:
-        print(f'{comparacao_1}')
+    for i in range(0, len(lista_dados) - 1):
+        comparacao_1 = lista_dados[i]
+        if comparacao_1 == lista_dados[i + 1]:
+            print(f'{comparacao_1}')
