845bd8c872a7a0c384cdde8097013d6ace5199e9
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-20 17:07:56 -0300

    Update Teste_020.py

--- Testes/Teste_020.py
+++ Testes/Teste_020.py
@@ -1,27 +1,30 @@
-lista_dados = [
-    'C:\\Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-01-31_14-12-49.PNG',
-    'C:\\Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-02_16-32-27.PNG',
-    'C:\\Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-07_20-36-40.PNG',
-    'C:\\Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-12_08-25-13.PNG',
-    'C:\\Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-01-31_14-12-49.PNG',
-    'C:\\Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-02_16-32-27.PNG',
-    'C:\\Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-07_20-36-40.PNG',
-    'C:\\Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-12_08-25-13.PNG'
-]
-
-lista_dados.sort()
+lista_dados = ['Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-01-31_14-12-49.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-02_16-32-27.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-07_20-36-40.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-12_08-25-13.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-01-31_14-12-49.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-02_16-32-27.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-07_20-36-40.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-12_08-25-13.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-01-31_14-12-49.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-02_16-32-27.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-07_20-36-40.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-12_08-25-13.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-01-31_14-12-49.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-02_16-32-27.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-07_20-36-40.PNG',
+               'Users\Thiago\OneDrive\Documentos\SimCity\Pictures ==> SPARK_2024-02-12_08-25-13.PNG']
 
 for valor in lista_dados:
     valor_arquivo = str(valor).split('==>')[-1].strip()
     valor_pasta = str(valor).split('==>')[0].strip()
 
-
 duplicado = dict()
 for valor in lista_dados:
-    if valor in duplicado:
-        duplicado[valor] += 1
+    if str(valor).split('==>')[-1].strip() in duplicado:
+        duplicado[str(valor).split('==>')[-1].strip()] += 1
     else:
-        duplicado[valor] = 1
+        duplicado[str(valor).split('==>')[-1].strip()] = 1
 for k, v in duplicado.items():
     if v > 1:
         print(f'{k} - {v}')
