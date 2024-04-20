062c2f03889bcbfcbe101d82d5cd511538473ff6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-20 17:22:12 -0300

    Update Teste_020.py

--- Testes/Teste_020.py
+++ Testes/Teste_020.py
@@ -1,24 +1,27 @@
 from tkinter.filedialog import askdirectory
 from pathlib import Path
-
+lista_da_busca = []
 lista_dados = []
 pasta_de_busca = Path(str(askdirectory()))
 
 for resultado_busca in pasta_de_busca.glob('**/*'):
     if resultado_busca.is_file():
-        lista_dados.append(resultado_busca)
-        print(resultado_busca)
+        lista_da_busca.append(resultado_busca)
 
-for valor in lista_dados:
+for valor in lista_da_busca:
     valor_arquivo = str(valor).split('\\')[-1].strip()
     valor_pasta = str(valor).split('\\')[:-2]
 
+
+    lista_dados = [[valor_pasta], [valor_arquivo]]
+
+    print(lista_dados)
 duplicado = dict()
 for valor in lista_dados:
-    if str(valor).split('==>')[-1].strip() in duplicado:
-        duplicado[str(valor).split('==>')[-1].strip()] += 1
+    if str(valor).split('\\')[-1].strip() in duplicado:
+        duplicado[str(valor).split('\\')[-1].strip()] += 1
     else:
-        duplicado[str(valor).split('==>')[-1].strip()] = 1
+        duplicado[str(valor).split('\\')[-1].strip()] = 1
 for k, v in duplicado.items():
     if v > 1:
         print(f'{k} - {v}')
