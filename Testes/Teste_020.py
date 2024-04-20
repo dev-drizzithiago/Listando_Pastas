0c4d09e7a7b604b789c7c52b57cb710bc1c4f107
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-20 20:59:16 -0300

    Update Teste_020.py

--- Testes/Teste_020.py
+++ Testes/Teste_020.py
@@ -38,36 +38,27 @@ for valor in lista_da_busca:
 
     lista_dados.append(str(valor).split('\\')[-1].strip())
 
-for indice in range(0, len(lista_dados)):
+for indice in range(0, len(lista_dados) - 2):
     valor_item = lista_dados[indice]
 
     if indice % 2 == 0:
         valor_diretorio = lista_dados[indice]
-        print(f'Indice:{indice} - {valor_diretorio}')
+        # print(f'Indice:{indice} - {valor_diretorio}')
     else:
-        print(f'Indice:{indice} - {valor_item}')
+        # print(f'Indice:{indice} - {valor_item}')
         caminho_origem = str(valor_diretorio + '/' + valor_item)
         arquivos = glob.glob(caminho_origem)
-        print(f'Caminho de origem: {caminho_origem}')
-        print('=-=' * 20)
+        # print(f'Caminho de origem: {caminho_origem}')
+        # print('=-=' * 20)
+    for j in range(1, len(valor_item) - 1):
+        if valor_item == lista_dados[j]:
+            print(f'{valor_item}')
 
     if valor_item in duplicado:
         duplicado[valor_item] += 1
-        try:
-            shutil.move(caminho_origem, caminho_destino)
-            print('Arquivo movido!')
-        except:
-            print('Não foi possível mover os arquivos para pasta de destino!')
     else:
         duplicado[valor_item] = 1
 
 for k, v in duplicado.items():
     if v > 1:
         print(f'Arquivos repetidos: {k}')
-
-print('=-=' * 20)
-
-for i in range(0, len(lista_dados) - 1):
-    comparacao_1 = lista_dados[i]
-    if comparacao_1 == lista_dados[i + 1]:
-        print(f'{comparacao_1}')
