c5499ece78031c9c8fa178f1154153db832d71a5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-20 20:05:16 -0300

    Update Teste_020.py

--- Testes/Teste_020.py
+++ Testes/Teste_020.py
@@ -1,10 +1,12 @@
-import os.path
+import os
 from tkinter.filedialog import askdirectory
 from pathlib import Path
 
 lista_da_busca = []
 lista_dados = []
 duplicado = dict()
+home = Path.home()
+caminho_direto_duplicado = Path(home, 'OneDrive', 'Documentos', 'Duplicados')
 pasta_de_busca = Path(str(askdirectory()))
 
 for resultado_busca in pasta_de_busca.glob('**/*'):
@@ -26,12 +28,12 @@ for valor in lista_da_busca:
 
 for indice in range(2, len(lista_dados)):
     valor_item = lista_dados[indice]
-
     print(f'Indice:{indice} - {valor_item}')
     if valor_item in duplicado:
         duplicado[valor_item] += 1
     else:
         duplicado[valor_item] = 1
+                
 for k, v in duplicado.items():
     if v > 1:
         print(f'{k} - {v}')
