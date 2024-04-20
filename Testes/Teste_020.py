fd6554d5f63d0f22b517078ede2857bea125fc99
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-20 13:29:23 -0300

    Update Teste_020.py

--- Testes/Teste_020.py
+++ Testes/Teste_020.py
@@ -1,6 +1,19 @@
-tempos = [0.5, 1.3, 2.5, 3.4, 0.5]
-comparacoes = []
-for i in range(0, len(tempos) - 1):
-    if tempos[i] == tempos[i + 1]:
-        comparacoes.append(tempos[i])
-print(comparacoes)
+
+lista_dados = [1, 1, 2, 3, 4, 5, 5, 6, 7, 8, 10]
+lista_dados.sort()
+
+duplicado = dict()
+for valor in lista_dados:
+    if valor in duplicado:
+        duplicado[valor] += 1
+    else:
+        duplicado[valor] = 1
+for k, v in duplicado.items():
+    if v > 1:
+        print(f'{k} - {v}')
+
+for i in range(0, len(lista_dados) - 1):
+    comparacao_1 = lista_dados[i]
+    for j in range(1, len(lista_dados) - 1):
+        if comparacao_1 != lista_dados[j]:
+            print(f'{lista_dados[j]}')
