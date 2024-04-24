aa7b3b19a7fd9aee0ee61dcfad30186998be50e8
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-24 12:07:15 -0300

    Update Listando_pasta_010.py

--- V_010/Listando_pasta_010.py
+++ V_010/Listando_pasta_010.py
@@ -1,7 +1,7 @@
 """#### Declaração de Modulos"""
 from tkinter.messagebox import showwarning, showinfo
 from tkinter.filedialog import askdirectory
-from janela_duplicidade import *
+# from janela_duplicidade import *
 from tkinter.ttk import *
 import tkinter as tk
 import os.path
@@ -50,7 +50,6 @@ class ProgramaPrincipal:
         self.janela_principal.title('V_010')
         self.janela_principal.geometry('1100x680+150+5')
         self.janela_principal.resizable(0, 0)
-
         # -=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
         """#### LabelFrame Principal"""
         self.label_frame_principal = tk.LabelFrame(self.janela_principal)
@@ -187,11 +186,12 @@ class ProgramaPrincipal:
         self.botao_limpar_checkbuttun.config(command=self.thread_botao_limpeza_checkbutton)
         self.botao_limpar_checkbuttun.pack(anchor='n', fill='both')
         # ______________________________________________________________________________________________________________
-        """# Botão limpeza lista de extensão"""
+        """# Botão verificar duplicidade"""
         self.botao_duplicidade = tk.Button(self.frame_label_duplicidade, text='Aplicar')
         self.botao_duplicidade.config(width=15, pady=5, padx=5, bg='#D3D3D3')
         self.botao_duplicidade.config(command=self.thread_botao_duplicidade_janela_principal, state=tk.DISABLED)
         self.botao_duplicidade.pack(anchor='n', fill='both')
+        # ______________________________________________________________________________________________________________
         # ______________________________________________________________________________________________________________
         """# Frame Label Listas de buscas"""
         self.frame_label_lista_resultado = tk.LabelFrame(self.label_frame_principal, text='Resultado das buscas')
@@ -247,6 +247,75 @@ class ProgramaPrincipal:
         """###### LOOP DA JANELA ######"""
         self.janela_principal.mainloop()
 
+    def janela_duplicidade(self):
+        """#### Janela de opções duplicados; só interno """
+        self.janela_opc_duplicidade = tk.Tk()
+        self.janela_opc_duplicidade.geometry('1000x600+200+50')
+        self.janela_opc_duplicidade.resizable(0, 0)
+        self.janela_opc_duplicidade.title('Verificando duplicidade!')
+
+        # ______________________________________________________________________________________________________________
+        """# Frame Principal DUPLICIDADE"""
+        self.frame_label_duplicidade = tk.LabelFrame(self.janela_opc_duplicidade, text='Verificando duplicidade!')
+        self.frame_label_duplicidade.config(width=900, height=580)
+        self.frame_label_duplicidade.pack(fill=tk.BOTH, pady=5, padx=5)
+
+        # ______________________________________________________________________________________________________________
+        """# Lista de resultado dos arquivos duplicados"""
+        self.var_result_duplicidade = tk.StringVar()
+        self.lista_result_duplicidade = tk.Listbox(self.frame_label_duplicidade)
+        self.lista_result_duplicidade.config(selectmode=tk.SINGLE, width=160, height=15)
+        self.lista_result_duplicidade.place(y=3, x=3)
+
+        # ______________________________________________________________________________________________________________
+        """# Criando a barra de rolagem para lista de resultado"""
+        self.barra_rolagem_lista_resultado_ducplicados = Scrollbar(self.janela_opc_duplicidade)
+        self.barra_rolagem_lista_resultado_ducplicados.config(orient='vertical')
+        self.barra_rolagem_lista_resultado_ducplicados.place(in_=self.lista_result_duplicidade)
+        self.barra_rolagem_lista_resultado_ducplicados.place(relx=1.0, relheight=1.0)
+        self.barra_rolagem_lista_resultado_ducplicados.place(bordermode='outside')
+        self.barra_rolagem_lista_resultado_ducplicados.config(command=self.lista_result_duplicidade.yview)
+        self.lista_result_duplicidade.config(yscrollcommand=self.barra_rolagem_lista_resultado_ducplicados.set)
+
+        # ______________________________________________________________________________________________________________
+        """Frame Superior duplicidade"""
+        self.frame_superior_dupli = tk.Frame(self.frame_label_duplicidade, bg='#C0C0C0', width=980, height=150)
+        self.frame_superior_dupli.place(y=250, x=3)
+
+        # ______________________________________________________________________________________________________________
+        """# Opcao check movendo os arquivos duplicados"""
+        self.var_opcao_move = tk.BooleanVar()
+        self.opcao_move = tk.Checkbutton(self.frame_superior_dupli, text='Mover arquivos duplicados', bg='#C0C0C0')
+        self.opcao_move.config(variable=self.var_opcao_move, pady=5, padx=5, bd=2)
+        self.opcao_move.place(y=5, x=5)
+
+        # ______________________________________________________________________________________________________________
+        """# Opção Deletar arquivos duplicados"""
+        self.var_opcao_delete = tk.BooleanVar()
+        self.opcao_delete = tk.Checkbutton(self.frame_superior_dupli, text='Deletar arquivos duplicados', bg='#C0C0C0')
+        self.opcao_delete.config(variable=self.var_opcao_delete, pady=5, padx=5, bd=2)
+        self.opcao_delete.place(y=30, x=5)
+
+        # ______________________________________________________________________________________________________________
+        self.frame_inferior_dupli = tk.Frame(self.frame_label_duplicidade, bg='#C0C0C0', width=980, height=150)
+        self.frame_inferior_dupli.place(y=407, x=3)
+
+        # ______________________________________________________________________________________________________________
+        """#### Frame botao """
+        self.frame_lbl_botao = tk.LabelFrame(self.frame_inferior_dupli, text='Processar', bg="#C0C0C0", pady=5, padx=5)
+        self.frame_lbl_botao.place(y=7, x=5)
+
+        # ______________________________________________________________________________________________________________
+        """#### Botoes de opcao"""
+        self.botao_aplica_opcao_check = tk.Button(self.frame_lbl_botao, text='Aplicar', bg='#C0C0C0', width=133)
+        self.botao_aplica_opcao_check.config(command=self.thread_opcao_check_botao)
+        self.botao_aplica_opcao_check.pack(anchor='center', pady=5, padx=5)
+
+        # ______________________________________________________________________________________________________________
+
+        """ Responsável por manter a janela de duplicidade ativa (padrão do tkinter)"""
+        self.janela_opc_duplicidade.mainloop()
+
     """##### THREADS DOS BOTÕES"""
     def thread_botao_inicio_da_busca(self):
         """
@@ -276,10 +345,23 @@ class ProgramaPrincipal:
         print(f'Iniciando THREAD tempo_precesso_busca')
         Thread(target=self.tempo_processo_busca).start()
 
-    """#### Processos de duplicidade"""
+    def thread_opcao_check_botao(self):
+        Thread(target=self.opcao_check_botao).start()
+
     def thread_botao_duplicidade_janela_principal(self):
         print(f'Iniciando THREAD botao_duplicidade')
-        Thread(target=valor_dados_busca(self.dados_do_processo_busca)).start()
+        Thread(target=self.botao_processo_duplicidade).start()
+
+    """# Processo de duplicidade"""
+
+    def opcao_check_botao(self):
+        resposta_move = self.var_opcao_move.get()
+        if resposta_move:
+            print('teste mover')
+
+        resposta_delete = self.var_opcao_delete.get()
+        if resposta_delete:
+            print('teste delete')
 
     """#### Sistema de combo e criaçãodo checkbutton"""
     def selecao_combo_extensao(self, *args):
@@ -503,6 +585,8 @@ class ProgramaPrincipal:
         self.diretorio_home = Path(askdirectory())
         self.lbl_pts_dest.config(text=f'Pasta de busca: [{self.diretorio_home}]', bg='#C0C0C0')
 
+    def botao_processo_duplicidade(self):
+        pass
     def botao_inicio_da_busca_principal(self):
         """
 
