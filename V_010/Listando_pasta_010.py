c5c25fb74f7f069a40c05168840349cca4bf42d3
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-25 09:33:28 -0300

    Update Listando_pasta_010.py

--- V_010/Listando_pasta_010.py
+++ V_010/Listando_pasta_010.py
@@ -621,20 +621,20 @@ class ProgramaPrincipal:
         except:
             showwarning("AVISO", "Não possui dados na lista 'elf.dados_do_processo_busca'")
 
-        try:
-            if valor_arquivo_da_busca in dict_duplicado:
-                dict_duplicado[valor_arquivo_da_busca] += 1
-                dict_duplicado['Diretorio'] = [valor_caminho_da_busca]
-            else:
-                dict_duplicado[valor_arquivo_da_busca] = 1
-        except:
-            showwarning("AVISO", 'Não foi possível calcular os arquivos duplicados')
+            try:
+                if valor_arquivo_da_busca in dict_duplicado:
+                    dict_duplicado[valor_arquivo_da_busca] += 1
+                    dict_duplicado['Diretorio'] = [valor_caminho_da_busca]
+                else:
+                    dict_duplicado[valor_arquivo_da_busca] = 1
+            except:
+                showwarning("AVISO", 'Não foi possível calcular os arquivos duplicados')
 
-        try:
-            for k, v in dict_duplicado.items():
-                self.lista_result_duplicidade.insert('end', f'{k}: {v}')
-        except UnboundLocalError:
-            showerror("ERROR", "Não existem dados para ser apresentados na lista!")
+            try:
+                for k, v in dict_duplicado.items():
+                    self.lista_result_duplicidade.insert('end', f'{k}: {v}')
+            except UnboundLocalError:
+                showerror("ERROR", "Não existem dados para ser apresentados na lista!")
 
     def opcao_check_botao(self):
         print('Iniciando "opcao_check_botao"')
