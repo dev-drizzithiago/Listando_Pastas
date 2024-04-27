45b52dbf277b5f4da90d886313d6007cc8c72ea5
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-26 23:23:46 -0300

    Update Listando_pasta_010.py

--- V_010/Listando_pasta_010.py
+++ V_010/Listando_pasta_010.py
@@ -728,7 +728,7 @@ class ProgramaPrincipal:
                         print(f'Arquivos {caminho_arquivo} movidos com sucesso! \n'
                               f'Pasta de destino: {caminho_destino}\n')
                     except shutil.Error:
-                        caminho_arquivo + f'Copy-{indice}'
+                        move(caminho_arquivo + str(f'Copy-{indice}'), caminho_destino)
                         showerror('AVISO', f'Arquivo {caminho_arquivo} já existe nessa pasta')
                 else:
                     unico_arquivo[hash_file] = caminho_arquivo
