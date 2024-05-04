7f6f817b227bb0264b3f56f2867d985bc629250a
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-05-03 22:36:54 -0300

    Create Teste_025.py

---
+++ Testes/Teste_025.py
@@ -0,0 +1,10 @@
+import subprocess
+import os
+
+caminho_exe = 'C:\\Users\\Thiago\\Downloads\\Youtube_V1\\Youtube_V1.exe'
+caminho_pdf = 'C:\\Users\\Thiago\\OneDrive\\Área de Trabalho\\Nova pasta\\bpe-51625347.pdf'
+# subprocess.run(caminho_exe)
+# subprocess.run(caminho_pdf)
+os.startfile(caminho_pdf)
+
+
