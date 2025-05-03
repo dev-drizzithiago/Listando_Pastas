1c745f60d819ae7edc2366c4b982c783d325d231
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-03 20:07:43 -0300

    Update Teste_26.py

--- Testes/Teste_26.py
+++ Testes/Teste_26.py
@@ -1,8 +1,18 @@
 import os
 import pathlib
+import subprocess
+import json
 
 
 PASTA_VIDEOS_AS_AVENTURAS_TITIN = pathlib.Path(r'\\muonline\Mini_CELERON\Videos\Classicos\As Aventuras de Tintin Completo')
 
 for item in os.listdir(PASTA_VIDEOS_AS_AVENTURAS_TITIN):
-    print(os.stat(rf'{PASTA_VIDEOS_AS_AVENTURAS_TITIN}\{item}').st_size)
+    # print(os.stat(rf'{PASTA_VIDEOS_AS_AVENTURAS_TITIN}\{item}').st_dev)
+
+    COMANDO_SHELL = f'ffprobe -v quiet -print_format json -show_format -show_streams {item}'
+
+    resultado = subprocess.run(COMANDO_SHELL, shell=True, capture_output=True, text=True)
+    metadados = json.loads(resultado.stdout)
+
+    print(json.dumps(metadados, indent=4))
+
