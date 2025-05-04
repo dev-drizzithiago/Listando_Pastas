eed3acf42258982d720aefdb28b7153d77145c83
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-04 12:23:42 -0300

    Update modulo_renomear_arquivos.py

--- V_010/modulo_renomear_arquivos.py
+++ V_010/modulo_renomear_arquivos.py
@@ -11,8 +11,6 @@ def renomeando_arquivos(PASTA_SELECIONADA):
         info = pymediainfo.MediaInfo.parse(CAMINHO_ABS_ORIGINAL)
         for track in info.tracks:
             if track.track_type == "General":
-                print(track.to_data())
-                # print(track.file_extension)
                 try:
                     os.rename(
                         CAMINHO_ABS_ORIGINAL, rf'{CAMINHO_ABS_MODIFICADO}\{indice}.novo_nome.{track.file_extension}'
@@ -22,7 +20,6 @@ def renomeando_arquivos(PASTA_SELECIONADA):
                     return False
 
         indice += 1
-        # print(CAMINHO_ABS_ORIGINAL)
 
 
 if __name__ == '__main__':
