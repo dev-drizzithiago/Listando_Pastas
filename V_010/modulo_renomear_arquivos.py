492b5a3a10c71f88836939d3a5619292265cb843
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2025-05-04 13:23:34 -0300

    Update modulo_renomear_arquivos.py

--- V_010/modulo_renomear_arquivos.py
+++ V_010/modulo_renomear_arquivos.py
@@ -1,18 +1,13 @@
 import os
 import pymediainfo
 
-from tkinter.filedialog import askdirectory
-PASTA_SELECIONADA_TESTE = r'\\muonline\Mini_CELERON\Videos\Novos\11 - 22 - 63 2016 - 1ª Temporada'
-ARQUIVO_PARA_RENOMEAR_TESTE = r'D:\Estudos\Python\GitHub\Listando_Pastas\V_010\lista_episodios_tintin.txt'
-
 def renomeando_arquivos(PASTA_SELECIONADA, ARQUIVO_PARA_RENOMEAR):
     indice = 1
-    lista_itens = []
     with open(ARQUIVO_PARA_RENOMEAR, 'r', encoding='utf8') as arquivo:
         for item_titulo in arquivo.readlines():
-            lista_itens.append(item_titulo.replace('\n', ''))
+            LISTA_ENTRADA_DADOS_RENOMAR_DADOS.append(item_titulo.replace('\n', ''))
 
-    print(lista_itens)
+    print(LISTA_ENTRADA_DADOS_RENOMAR_DADOS)
 
     for item in os.listdir(PASTA_SELECIONADA):
 
@@ -29,9 +24,11 @@ def renomeando_arquivos(PASTA_SELECIONADA, ARQUIVO_PARA_RENOMEAR):
                     return True
                 except:
                     return False
-
         indice += 1
 
 
+LISTA_ENTRADA_DADOS_RENOMAR_DADOS = []
+PASTA_SELECIONADA_TESTE = r'\\muonline\Mini_CELERON\Videos\Novos\11 - 22 - 63 2016 - 1ª Temporada'
+ARQUIVO_PARA_RENOMEAR_TESTE = r'D:\Estudos\Python\GitHub\Listando_Pastas\V_010\lista_episodios_tintin.txt'
 if __name__ == '__main__':
     renomeando_arquivos(PASTA_SELECIONADA_TESTE, ARQUIVO_PARA_RENOMEAR_TESTE)
